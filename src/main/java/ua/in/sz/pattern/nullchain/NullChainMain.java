af620ec656023324a1a004bde0e69715028e27d8
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-11-19 01:48:27 +0200

    Interpreter Design Pattern Example Class Diagram

---
+++ src/main/java/ua/in/sz/pattern/nullchain/NullChainMain.java
@@ -0,0 +1,75 @@
+package ua.in.sz.pattern.nullchain;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+/**
+ * Null object pattern
+ * Law of Demeter
+ * 		Each unit should have only limited knowledge about other units: only units "closely" related to the current unit.
+ * 		Each unit should only talk to its friends; don't talk to strangers.
+ * 		Only talk to your immediate friends.
+ */
+@Slf4j
+public class NullChainMain {
+	public static void main(String[] args) {
+		A first = new A();
+//		A first = null;
+		String text = Optional.ofNullable(first)
+				.map(I::next)
+				.map(I::next)
+				.map(I::next)
+				.map(I::text)
+				.orElse(null);
+
+		text = opt(() -> first.next().next().next().text());
+		log.info("Text: {}", text);
+	}
+
+	private static <T> T opt(Supplier<T> statement) {
+		try {
+			return statement.get();
+		} catch (NullPointerException exc) {
+			log.warn("npe");
+			return null;
+		}
+	}
+
+	interface I {
+		default I next() {
+			return null;
+		}
+
+		default String text() {
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	static class A implements I {
+		public I next() {
+			return new B();
+//			return null;
+		}
+	}
+
+	static class B implements I {
+		public C next() {
+			return new C();
+//			return null;
+		}
+	}
+
+	static class C implements I {
+		public D next() {
+			return new D();
+		}
+	}
+
+	static class D implements I {
+		public String text() {
+			return "Text";
+		}
+	}
+}
