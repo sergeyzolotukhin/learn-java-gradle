873ee7468fabb14960ee348260c1f3fd5b763bb2
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-11-21 21:01:26 +0200

    Interpreter Design Pattern Example Class Diagram

---
+++ src/main/java/ua/in/sz/pattern/iterator/next/IteratorMain.java
@@ -0,0 +1,15 @@
+package ua.in.sz.pattern.iterator.next;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class IteratorMain {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+
+		String next = list.iterator().next();
+	}
+}
