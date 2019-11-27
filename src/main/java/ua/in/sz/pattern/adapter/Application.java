15ff0726b087d02442757911af9164125a4d248a
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-11-27 22:22:40 +0200

    Adapter Design Pattern

--- src/main/java/ua/in/sz/pattern/adapter/Application.java
+++ src/main/java/ua/in/sz/pattern/adapter/Application.java
@@ -1,23 +1,29 @@
 package ua.in.sz.pattern.adapter;
 
-import com.google.common.collect.ImmutableList;
 import lombok.extern.slf4j.Slf4j;
+import ua.in.sz.pattern.adapter.adapter.DelegateShapeAdapter;
 import ua.in.sz.pattern.adapter.ownapi.Drawing;
 import ua.in.sz.pattern.adapter.ownapi.Rectangle;
 import ua.in.sz.pattern.adapter.thirdpartyapi.CircleShape;
 import ua.in.sz.pattern.adapter.thirdpartyapi.DrawingShape;
 
+import java.util.Arrays;
+
+import static java.util.Collections.singletonList;
+
 @Slf4j
 public class Application {
 	public static void main(String[] args) {
 		Drawing ownDrawing = Drawing.builder()
-				.shapes(ImmutableList.of(new Rectangle()))
+				.shapes(Arrays.asList(
+						new Rectangle(),
+						new DelegateShapeAdapter(new CircleShape())))
 				.build();
 
 		ownDrawing.render();
 
 		DrawingShape thirdDrawing = DrawingShape.builder()
-				.shapes(ImmutableList.of(new CircleShape()))
+				.shapes(singletonList(new CircleShape()))
 				.build();
 
 		thirdDrawing.draw();
---
+++ src/main/java/ua/in/sz/pattern/adapter/adapter/DelegateShapeAdapter.java
@@ -0,0 +1,17 @@
+package ua.in.sz.pattern.adapter.adapter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ua.in.sz.pattern.adapter.ownapi.Shape;
+import ua.in.sz.pattern.adapter.thirdpartyapi.GeometricShape;
+
+@Slf4j
+@RequiredArgsConstructor
+public class DelegateShapeAdapter implements Shape {
+	private final GeometricShape delegate;
+
+	@Override
+	public void render() {
+		delegate.draw();
+	}
+}
