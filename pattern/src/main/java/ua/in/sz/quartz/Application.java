15810b347e588ba3c197cfff2d5e77aaaf4df165
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-12-03 00:53:46 +0200

    Quartz

--- pattern/src/main/java/ua/in/sz/quartz/Application.java
+++ pattern/src/main/java/ua/in/sz/quartz/Application.java
@@ -5,7 +5,6 @@ import org.quartz.Job;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SchedulerFactory;
