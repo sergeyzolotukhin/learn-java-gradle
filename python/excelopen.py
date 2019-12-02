c72db726fa2e05c04e92cceb9478e004fcb5ec7f
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-12-02 23:53:31 +0200

    Guava cache

--- python/excelopen.py
+++
@@ -1,6 +0,0 @@
-import os, os.path
-import win32com.client
-
-xl=win32com.client.Dispatch("Excel.Application")
-xl.Visible = True
-xl.Workbooks.Open(os.path.abspath("k:\chrome\Factorio_science.xls"), ReadOnly=1)
