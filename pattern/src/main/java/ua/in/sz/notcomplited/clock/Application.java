5db202fb1a028e4288b1850c8a4c58cf6d7cdd03
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-12-03 22:35:19 +0200

    Java 8 data time

--- pattern/src/main/java/ua/in/sz/notcomplited/clock/Application.java
+++ pattern/src/main/java/ua/in/sz/notcomplited/clock/Application.java
@@ -5,11 +5,11 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.Clock;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,20 +24,16 @@ public class Application {
 		processes.add(new Process(LocalDate.of(2019, 12, 3)));
 		processes.add(new Process(LocalDate.of(2019, 12, 15)));
 
-		LocalDateTime current = LocalDate.of(2019, 11, 10).atStartOfDay();
+		LocalDateTime start = LocalDate.of(2019, 11, 10).atStartOfDay();
+		Clock baseClock = Clock.fixed(start.toInstant(ZoneOffset.UTC), ZoneId.systemDefault());
 
-		for (int i = 0; i < 31*2*24; i++) {
-			current = current.plus(4, ChronoUnit.HOURS);
+		for (int i = 0; i < 31 * 2 * 24; i++) {
+			clock = Clock.offset(baseClock, Duration.ofHours(4 * i));
 
-			clock = Clock.fixed(current.toInstant(ZoneOffset.UTC), ZoneId.systemDefault());
-
-			final LocalDateTime c = current;
-//			log.info("Current: {}", c);
 			processes.stream()
 					.filter(Application::isDueDateNow)
-					.forEach(p -> log.info("Process current {} due date: {}", c, p.getDueDate()));
+					.forEach(p -> log.info("Process current {} due date: {}", LocalDateTime.now(clock), p.getDueDate()));
 		}
-
 	}
 
 	private static boolean isDueDateNow(Process process) {
