d10fa1bfde1c8a5405e6f0074997184cfb35b162
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-12-04 21:20:23 +0200

    Java 8 stream

--- pattern/src/test/java/ua/in/sz/notcomplited/streams/ApplicationTest.java
+++ pattern/src/test/java/ua/in/sz/notcomplited/streams/ApplicationTest.java
@@ -1,5 +1,6 @@
 package ua.in.sz.notcomplited.streams;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,18 +9,30 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class ApplicationTest {
-	@Test
-	void groupingBy() {
-		final Dto[] dtos = {
+
+	Dto[] dtos;
+
+	@BeforeEach
+	private void setup() {
+		dtos = new Dto[]{
 				Dto.of(1, "One"),
 				Dto.of(1, "One *"),
 				Dto.of(2, "Two"),
 				Dto.of(3, "Three"),
-				Dto.of(3, "Three *")
-		};
+				Dto.of(3, "Three *")};
+	}
 
+	@Test
+	void groupingBy() {
 		Map<Integer, List<Dto>> index = Stream.of(dtos).collect(Collectors.groupingBy(Dto::getCode));
 
 		index.forEach((key, value) -> System.out.println(String.format("%s=%s", key, value.size())));
+	}
+
+	@Test
+	void partitioningBy() {
+		Map<Boolean, List<Dto>> partitions = Stream.of(dtos).collect(Collectors.partitioningBy(i -> i.getCode() <= 2));
+
+		partitions.forEach((key, value) -> System.out.println(String.format("%s=%s", key, value.size())));
 	}
 }
