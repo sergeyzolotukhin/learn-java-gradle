9efdbc2abbda5e8fce460a97a73b5923d7bd6410
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-12-06 22:36:54 +0200

    Locale

---
+++ pattern/src/main/java/ua/in/sz/logging/Application.java
@@ -0,0 +1,19 @@
+package ua.in.sz.logging;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+public class Application {
+	public static void main(String[] args) throws IOException {
+		log.info("start execution");
+
+		System.getProperties().load(Application.class.getResourceAsStream("/application.properties"));
+
+		log.info("Author: {}", System.getProperty("author"));
+
+
+		log.info("end execution");
+	}
+}
