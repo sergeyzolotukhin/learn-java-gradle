6bcbe511329a41b57295b1adb866d28463c66fd0
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-12-07 00:12:56 +0200

    Locale

---
+++ pattern/src/main/java/ua/in/sz/logging/MdcFilter.java
@@ -0,0 +1,22 @@
+package ua.in.sz.logging;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.filter.AbstractMatcherFilter;
+import ch.qos.logback.core.spi.FilterReply;
+import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+
+@Setter
+public class MdcFilter extends AbstractMatcherFilter<ILoggingEvent> {
+
+	String feature = StringUtils.EMPTY;
+
+	@Override
+	public FilterReply decide(ILoggingEvent event) {
+		if (feature.equals(event.getMDCPropertyMap().get(Mdc.FEATURE))) {
+			return onMatch;
+		} else {
+			return onMismatch;
+		}
+	}
+}
