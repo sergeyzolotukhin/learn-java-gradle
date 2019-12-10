374fe899c18b6b98843c29ba8394f960ff22f386
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-12-10 21:56:18 +0200

    Spring dev tools restart when change application.properties

--- pattern/src/main/java/ua/in/sz/guavacache/SlowCacheLoader.java
+++ pattern/src/main/java/ua/in/sz/guavacache/SlowCacheLoader.java
@@ -2,12 +2,11 @@ package ua.in.sz.guavacache;
 
 import com.google.common.cache.CacheLoader;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.lang.NonNull;
 
 @Slf4j
 public class SlowCacheLoader extends CacheLoader<String, String> {
 	@Override
-	public String load(@NonNull String key) throws Exception {
+	public String load(String key) throws Exception {
 		log.info("Loading ... ");
 
 		Thread.sleep(2_000);
