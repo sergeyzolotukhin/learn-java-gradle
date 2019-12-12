b15d551e6ae51db208c49d2f8d66cc720238f8cd
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-12-12 22:28:08 +0200

    Method references

---
+++ pattern/src/main/java/ua/in/sz/javacore/comparatorchain/Application.java
@@ -0,0 +1,38 @@
+package ua.in.sz.javacore.comparatorchain;
+
+import com.google.common.collect.ImmutableList;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class Application {
+	public static void main(String[] args) {
+		List<Dto> list = ImmutableList.of(
+				Dto.builder().name("B1").code("C1").build(),
+				Dto.builder().name("A1").code("C2").build(),
+				Dto.builder().name("A1").code("C1").build()
+		);
+
+		String result = list.stream()
+				.sorted(Comparator.comparing(Dto::getName)
+						.thenComparing(Dto::getCode))
+				.map(Dto::toString)
+				.collect(Collectors.joining(", "));
+
+		log.info("Sorted: {}", result);
+	}
+
+	@Builder
+	@Getter
+	@ToString
+	private static class Dto {
+		private String name;
+		private String code;
+	}
+}
