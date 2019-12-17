49f86779ff83d4eefe4f956db116620c03e47115
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-12-18 00:09:39 +0200

    Filter DAO

--- spring-hibernate-search/src/main/java/ua/in/sz/spring/export/ScheduleExportService.java
+++ spring-hibernate-search/src/main/java/ua/in/sz/spring/export/ScheduleExportService.java
@@ -1,6 +1,8 @@
 package ua.in.sz.spring.export;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 import ua.in.sz.spring.filter.FilterDao;
 
@@ -8,10 +10,16 @@ import java.util.stream.Stream;
 
 @Slf4j
 @Service
-public class ScheduleExportService {
-	private FilterDao filterDao;
+public class ScheduleExportService implements CommandLineRunner {
+	private final FilterDao filterDao;
 
-	public void export() {
+	@Autowired
+	public ScheduleExportService(FilterDao filterDao) {
+		this.filterDao = filterDao;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 		ScheduleExportFilter filter = new ScheduleExportFilter();
 
 		long count = filterDao.count(filter);
