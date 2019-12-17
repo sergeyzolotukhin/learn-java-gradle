afad10e3533f295c9cf634e07da79b37d17a3fee
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-12-17 23:47:02 +0200

    Filter DAO

--- spring-hibernate-search/src/main/java/ua/in/sz/spring/export/ScheduleDaoImpl.java
+++ spring-hibernate-search/src/main/java/ua/in/sz/spring/export/ScheduleDaoImpl.java
@@ -1,17 +1,28 @@
 package ua.in.sz.spring.export;
 
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ua.in.sz.spring.filter.Filter;
 import ua.in.sz.spring.filter.FilterDao;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
+@Slf4j
 @Repository
 public class ScheduleDaoImpl implements FilterDao {
 
+    private final EntityManager entityManager;
+
     private SessionFactory sessionFactory;
+
+    @Autowired
+    public ScheduleDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     protected Session getSession() {
         return sessionFactory.getCurrentSession();
--- spring-hibernate-search/src/main/java/ua/in/sz/spring/export/ScheduleEntity.java
+++ spring-hibernate-search/src/main/java/ua/in/sz/spring/export/ScheduleEntity.java
@@ -1,4 +1,18 @@
 package ua.in.sz.spring.export;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class ScheduleEntity {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
 }
