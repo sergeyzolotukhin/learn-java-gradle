c9f74ae38d3882f1be86a4a573f166960bbad2db
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-12-23 21:23:37 +0200

    Boot test

---
+++ spring-dev-tools/src/test/java/ua/in/sz/spring/ApplicationTest.java
@@ -0,0 +1,15 @@
+package ua.in.sz.spring;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+//@SpringBootTest
+class ApplicationTest {
+	@Test
+	public void contextLoads() {
+
+	}
+}
