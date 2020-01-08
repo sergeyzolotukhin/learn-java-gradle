aa422c99db242cb5b8c77e2b35585444b985ed76
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-01-08 05:34:34 +0200

    Camel

--- spring-camel/src/main/java/ua/in/sz/pattern/spring/camel/Application.java
+++ spring-camel/src/main/java/ua/in/sz/pattern/spring/camel/Application.java
@@ -1,39 +1,36 @@
 package ua.in.sz.pattern.spring.camel;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ftpserver.FtpServer;
-import org.apache.ftpserver.FtpServerFactory;
-import org.apache.ftpserver.usermanager.ClearTextPasswordEncryptor;
-import org.apache.ftpserver.usermanager.impl.PropertiesUserManager;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.ImportResource;
 
-import java.io.File;
+import java.util.List;
 
 @Slf4j
 @SpringBootApplication
-public class Application {
+@ImportResource({"classpath:spring/ftp-servers.xml"})
+public class Application implements ApplicationListener<ApplicationReadyEvent> {
     private static final String ADMIN_USER_NAME = "admin";
 
-    @Value("classpath:users.properties")
-    private File usersFile;
+	@Autowired(required = false)
+	@SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
+    private List<FtpServer> ftpServers;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public SpringFtpServer ftpServer() {
-		FtpServerFactory factory = new FtpServerFactory();
-		factory.setUserManager(ftpUserManager());
-		FtpServer server = factory.createServer();
-
-		return new SpringFtpServer(server);
+	@Override
+	@SneakyThrows
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+		for (FtpServer server : ftpServers) {
+			server.start();
+		}
 	}
-
-    private PropertiesUserManager ftpUserManager() {
-        return new PropertiesUserManager(new ClearTextPasswordEncryptor(), usersFile, ADMIN_USER_NAME);
-    }
 }
