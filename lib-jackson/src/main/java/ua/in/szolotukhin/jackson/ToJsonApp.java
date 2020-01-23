f93138237ad3402f88aa32df22ee027c6bbe6972
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-01-23 23:18:23 +0200

    Jackson

---
+++ lib-jackson/src/main/java/ua/in/szolotukhin/jackson/ToJsonApp.java
@@ -0,0 +1,32 @@
+package ua.in.szolotukhin.jackson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import ua.in.szolotukhin.jackson.model.SchedulesFilter;
+import ua.in.szolotukhin.jackson.model.SchedulesRowDataProvider;
+
+import java.io.IOException;
+
+import static java.util.Collections.singletonList;
+
+@Slf4j
+public class ToJsonApp {
+
+	public static void main(String[] args) throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.writerWithDefaultPrettyPrinter();
+
+		SchedulesFilter filter = SchedulesFilter.builder()
+				.scheduleTypes(singletonList("RTBM"))
+				.activeVersion(true)
+				.build();
+
+		SchedulesRowDataProvider dataProvider = SchedulesRowDataProvider.builder()
+				.filter(filter)
+				.build();
+
+		String result = mapper.writeValueAsString(dataProvider);
+
+		log.info("{}", result);
+	}
+}
--- lib-jackson/src/main/java/ua/in/szolotukhin/jackson/model/SchedulesRowDataProvider.java
+++ lib-jackson/src/main/java/ua/in/szolotukhin/jackson/model/SchedulesRowDataProvider.java
@@ -1,4 +1,4 @@
-package ua.in.szolotukhin.jackson;
+package ua.in.szolotukhin.jackson.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
