8cc00f6175cbcc33d299de758290839c9a4dc258
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-01-23 23:55:43 +0200

    Jackson

--- lib-jackson/src/main/java/ua/in/szolotukhin/jackson/model/SchedulesFilter.java
+++ lib-jackson/src/main/java/ua/in/szolotukhin/jackson/model/SchedulesFilter.java
@@ -1,23 +1,44 @@
 package ua.in.szolotukhin.jackson.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+@Slf4j
 @Builder
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor
+//@NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SchedulesFilter {
 	private List<String> scheduleTypes;
 	private Boolean activeVersion;
+
+	public SchedulesFilter() {
+		log.info("default constructor");
+	}
+
+	@JsonCreator
+	public static SchedulesFilter createSchedulesFilter(
+			@JsonProperty("activeVersion") Boolean activeVersion,
+			@JsonProperty("scheduleTypes") List<String> scheduleTypes
+	) {
+		log.info("factory method invoked");
+
+		SchedulesFilter employee = new SchedulesFilter();
+		employee.scheduleTypes = scheduleTypes;
+		employee.activeVersion = activeVersion;
+		return employee;
+	}
 }
