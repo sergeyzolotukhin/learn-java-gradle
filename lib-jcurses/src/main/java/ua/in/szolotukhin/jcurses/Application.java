dfc42792c652d94813c4753d38d764649c6f325e
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-01-24 02:19:44 +0200

    jcurses source

--- lib-jcurses/src/main/java/ua/in/szolotukhin/jcurses/Application.java
+++ lib-jcurses/src/main/java/ua/in/szolotukhin/jcurses/Application.java
@@ -3,6 +3,7 @@ package ua.in.szolotukhin.jcurses;
 import jcurses.system.CharColor;
 import jcurses.widgets.DefaultLayoutManager;
 import jcurses.widgets.Label;
+import jcurses.widgets.TextArea;
 import jcurses.widgets.WidgetsConstants;
 import jcurses.widgets.Window;
 import lombok.extern.slf4j.Slf4j;
@@ -12,16 +13,27 @@ public class Application {
 	public static void main(String[] args) throws InterruptedException {
 		log.info("lib path: [{}]", System.getProperty("java.library.path"));
 
-		Window w = new Window(100, 100, true, "Hai hai!");
-		Label label = new Label("Hai hai!", new CharColor(CharColor.BLACK, CharColor.GREEN));
+		Window w = new Window(100, 50, true, "Hai hai!");
+		TextArea textArea = new TextArea();
 
 		DefaultLayoutManager mgr = new DefaultLayoutManager();
 		mgr.bindToContainer(w.getRootPanel());
-		mgr.addWidget(label, 0, 0, 100, 100, WidgetsConstants.ALIGNMENT_CENTER, WidgetsConstants.ALIGNMENT_CENTER);
+		mgr.addWidget(textArea, 2, 2, 50, 20,
+				WidgetsConstants.ALIGNMENT_CENTER, WidgetsConstants.ALIGNMENT_CENTER);
+
+		String text = "The runtime configuration has been deprecated for resolution. \n";
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 100; i++) {
+			sb.append(text);
+		}
+
+		textArea.setText(sb.toString());
+
 		w.show();
 
 		Thread.currentThread();
-		Thread.sleep(3000);
+		Thread.sleep(30000);
 
 		w.close();
 	}
