1b92f7c29d1f6e967e88be67d5e9364f3eeb4b28
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-01-24 05:28:58 +0200

    jcurses source

--- lib-jcurses/src/main/java/ua/in/szolotukhin/jcurses/Application.java
+++ lib-jcurses/src/main/java/ua/in/szolotukhin/jcurses/Application.java
@@ -1,6 +1,7 @@
 package ua.in.szolotukhin.jcurses;
 
 import jcurses.widgets.DefaultLayoutManager;
+import jcurses.widgets.MenuList;
 import jcurses.widgets.TextArea;
 import jcurses.widgets.WidgetsConstants;
 import jcurses.widgets.Window;
@@ -16,8 +17,19 @@ public class Application {
 
 		DefaultLayoutManager mgr = new DefaultLayoutManager();
 		mgr.bindToContainer(w.getRootPanel());
+
+
+		MenuList menu = new MenuList();
+		menu.add("Command 1");
+		menu.add("Command 2");
+		menu.add("Command 3");
+		menu.select(1);
+
 		mgr.addWidget(textArea, 2, 2, 50, 20,
 				WidgetsConstants.ALIGNMENT_CENTER, WidgetsConstants.ALIGNMENT_CENTER);
+
+		mgr.addWidget(menu, 0, 0, 50, 20,
+				WidgetsConstants.ALIGNMENT_LEFT, WidgetsConstants.ALIGNMENT_RIGHT);
 
 		String text = "The runtime configuration has been deprecated for resolution. \n";
 
