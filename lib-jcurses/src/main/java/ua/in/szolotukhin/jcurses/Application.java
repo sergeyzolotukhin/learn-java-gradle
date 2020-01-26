e0f9aa8fec9dd5200dbdc66d61e96b097d92b795
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-01-26 04:43:27 +0200

    jcurses source

--- lib-jcurses/src/main/java/ua/in/szolotukhin/jcurses/Application.java
+++ lib-jcurses/src/main/java/ua/in/szolotukhin/jcurses/Application.java
@@ -4,25 +4,36 @@ import jcurses.system.Toolkit;
 import jcurses.widgets.container.Panel;
 import jcurses.widgets.window.Window;
 import lombok.extern.slf4j.Slf4j;
-import org.fusesource.jansi.Ansi;
 import org.fusesource.jansi.AnsiConsole;
-
-import static org.fusesource.jansi.Ansi.Color.GREEN;
-import static org.fusesource.jansi.Ansi.Color.RED;
-import static org.fusesource.jansi.Ansi.ansi;
 
 @Slf4j
 public class Application {
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("jansi.passthrough", "true");
+//		System.setProperty("jansi.passthrough", "true");
 
-		AnsiConsole.systemInstall();
-		Ansi hello = ansi().eraseScreen().fg(RED).a("Hello").fg(GREEN).a(" World").reset();
-		System.out.println(hello);
+//		AnsiConsole.systemInstall();
+//		Ansi hello = ansi().eraseScreen().fg(RED).a("Hello").fg(GREEN).a(" World").reset();
+//		System.out.println(hello);
 
-		System.out.println("\u001B[31mHello 2\u001B[32mSerhij");
+//		System.out.println("\u001B[31mHello");
 
-		AnsiConsole.systemUninstall();
+		plainWindow();
+//		AnsiConsole.systemUninstall();
+	}
+
+	private static final String FG_RED = "\u001B[31m";
+
+	private static final String LU = "\u2554";
+	private static final String V = "\u2550";
+	private static final String RU = "\u2557";
+	private static final String H = "\u2551";
+	private static final String LD = "\u255A";
+	private static final String RD = "\u255D";
+
+	private static void plainWindow() {
+		System.out.println(FG_RED + LU + V + RU);
+		System.out.println(H + " " + H);
+		System.out.println(LD + V + RD);
 	}
 
 	private static void showWindow() throws InterruptedException {
