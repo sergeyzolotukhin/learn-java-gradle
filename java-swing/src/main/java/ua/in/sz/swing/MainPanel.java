fe7fab83c9cba7978e203889c34d1ef2e4a2bf87
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-01-30 01:45:36 +0200

    jcurses source

--- java-swing/src/main/java/ua/in/sz/swing/MainPanel.java
+++ java-swing/src/main/java/ua/in/sz/swing/MainPanel.java
@@ -1,17 +1,38 @@
 package ua.in.sz.swing;
 
 import lombok.extern.slf4j.Slf4j;
+import ua.in.sz.swing.model.Rect;
 
 import javax.swing.*;
 import java.awt.*;
 
 @Slf4j
 public class MainPanel extends JPanel {
+	private Graphics2D g2d;
+
 	@Override
 	public void paint(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
+		g2d = (Graphics2D) g;
 
-		g2d.setColor(Color.BLUE);
-		g2d.fillRect(100, 50, 60, 80);
+		Rect rectA = Rect.of(0, 0, 100, 100);
+		Rect rectB = Rect.of(10, 10, 100, 100);
+		Rect rectC = rectA.intersection(rectB);
+		Rect rectD = rectA.union(rectB);
+
+		draw(rectD, Color.GREEN);
+		draw(rectA, Color.BLUE);
+		draw(rectB, Color.RED);
+		draw(rectC, Color.YELLOW);
+	}
+
+	private void draw(Rect rectB, Color color) {
+		int x = rectB.getX();
+		int y = rectB.getY();
+		int w = rectB.getWidth();
+		int h = rectB.getHeight();
+
+		g2d.setColor(color);
+		g2d.fillRect(x, y, w, h);
+//		g2d.drawRect(x, y, w, h);
 	}
 }
---
+++ java-swing/src/main/java/ua/in/sz/swing/model/Rect.java
@@ -0,0 +1,35 @@
+package ua.in.sz.swing.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@AllArgsConstructor(staticName = "of")
+public class Rect {
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+
+	public Rect intersection(Rect r) {
+		int x1 = Math.max(x, r.x);
+		int y1 = Math.max(y, r.y);
+
+		int x2 = Math.min(x + width, r.x + r.width);
+		int y2 = Math.min(y + height, r.y + r.height);
+
+		return new Rect(x1, y1, x2 - x1, y2 - y1);
+	}
+
+	public Rect union(Rect r) {
+		int x1 = Math.min(x, r.x);
+		int y1 = Math.min(y, r.y);
+
+		int x2 = Math.max(x + width, r.x + r.width);
+		int y2 = Math.max(y + height, r.y + r.height);
+		
+		return new Rect(x1, y1, x2 - x1, y2 - y1);
+	}
+}
