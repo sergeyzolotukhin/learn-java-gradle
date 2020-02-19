8dcf6629163cff4b7c0d0fd85c7c633d28519e3a
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-02-19 21:42:34 +0200

    spring tx

--- spring-transactional/src/main/java/ua/in/sz/springtx/ApplicationRunner.java
+++ spring-transactional/src/main/java/ua/in/sz/springtx/ApplicationRunner.java
@@ -6,6 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+import java.util.List;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -14,7 +17,10 @@ public class ApplicationRunner implements CommandLineRunner {
 	private final NamedParameterJdbcTemplate template;
 
 	@Override
-	public void run(String... args) throws Exception {
-		log.info("Running ...");
+	public void run(String... args) {
+		List<String> names = template.queryForList(
+				"select EMPLOYEE_NAME from EMPLOYEE", Collections.EMPTY_MAP, String.class);
+
+		names.forEach(name -> log.info("Employer name: [{}]", name));
 	}
 }
