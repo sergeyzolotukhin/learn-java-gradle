2f9a2656c76dc8c7d84b7dd51592e2e88bc2587d
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-03-27 20:28:52 +0200

    timezone

--- pattern/src/test/java/ua/in/sz/javacore/timezone/ApplicationTest.java
+++ pattern/src/test/java/ua/in/sz/javacore/timezone/ApplicationTest.java
@@ -3,6 +3,7 @@ package ua.in.sz.javacore.timezone;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.TimeZone;
@@ -41,5 +42,17 @@ class ApplicationTest {
 
 		LocalDateTime dateTime1 = LocalDateTime.of(2020, 1, 1, 0, 0, 0);
 		log.info("Date time: [{}]", dateTime1);
+	}
+
+	@Test
+	void dateTimeLipe() {
+		log.info("Timezone: [{}]", format(TimeZone.getDefault()));
+
+		LocalDateTime dateTime = LocalDateTime.of(2021, 1, 1, 0, 0, 0);
+		LocalDateTime dateTime1 = dateTime.plusSeconds(60);
+		LocalDateTime dateTime2 = dateTime.plusMinutes(1);
+		LocalDateTime dateTime3 = dateTime.plus(Duration.ofMinutes(1));
+
+		log.info("Date time: [{}] => [{}] => [{}] => [{}]", dateTime, dateTime1, dateTime2, dateTime3);
 	}
 }
