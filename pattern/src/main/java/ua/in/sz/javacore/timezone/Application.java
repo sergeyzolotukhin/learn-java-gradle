8c75856d0cc42ac2e473af2f858a85ada4723759
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-03-27 20:03:35 +0200

    timezone

--- pattern/src/main/java/ua/in/sz/javacore/timezone/Application.java
+++ pattern/src/main/java/ua/in/sz/javacore/timezone/Application.java
@@ -2,6 +2,8 @@ package ua.in.sz.javacore.timezone;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.Arrays;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
@@ -12,14 +14,21 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class Application {
 	public static void main(String[] args) {
-
-		Arrays.stream(TimeZone.getAvailableIDs()).forEach(id ->
-				log.info("Timezone [{}]", displayTimeZone(TimeZone.getTimeZone(id))));
-
-
 		log.info("Timezone: [{}]", displayTimeZone(TimeZone.getDefault()));
 
-//		TimeZone.setDefault(TimeZone.getTimeZone("America/Los_Angeles"));
+		LocalDateTime dateTime = LocalDateTime.of(2020, 1, 1, 0, 0, 0);
+		log.info("Date time: [{}]", dateTime);
+
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Moscow"));
+		log.info("Timezone: [{}]", displayTimeZone(TimeZone.getDefault()));
+
+		LocalDateTime dateTime1 = LocalDateTime.of(2020, 1, 1, 0, 0, 0);
+		log.info("Date time: [{}]", dateTime1);
+	}
+
+	private static void helpTimeZone() {
+		Arrays.stream(TimeZone.getAvailableIDs()).forEach(id ->
+				log.info("Timezone [{}]", displayTimeZone(TimeZone.getTimeZone(id))));
 	}
 
 	private static String displayTimeZone(TimeZone tz) {
