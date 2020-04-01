a824d41adf05591a2a10bbd5031c3ff1153c9e24
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-01 03:54:08 +0300

    strategy

--- spring-quartz/src/main/java/ua/in/sz/spring/Application.java
+++ spring-quartz/src/main/java/ua/in/sz/spring/Application.java
@@ -1,10 +1,17 @@
 package ua.in.sz.spring;
 
 import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 
 @Slf4j
 @Configuration
@@ -13,11 +20,23 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
 	public static void main(String[] args) throws InterruptedException {
 		log.info("Start");
-
 		SpringApplication.run(Application.class);
-
-		Thread.sleep(10_000);
-
 		log.info("End");
+	}
+
+	@Bean
+	public JobDetail jobDetail() {
+		return JobBuilder.newJob().ofType(SampleJob.class)
+				.storeDurably()
+				.withIdentity("Qrtz_Job_Detail")
+				.build();
+	}
+
+	@Bean
+	public Trigger trigger(JobDetail job) {
+		return TriggerBuilder.newTrigger().forJob(job)
+				.withIdentity("Qrtz_Trigger")
+				.withSchedule(simpleSchedule().repeatForever().withIntervalInSeconds(2))
+				.build();
 	}
 }
