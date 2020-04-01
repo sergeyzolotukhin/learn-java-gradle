c4dd9b0d7af36d5af06fd161da3ee108d89f50d7
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-01 04:16:39 +0300

    strategy

--- spring-quartz/src/main/java/ua/in/sz/spring/SampleJob.java
+++ spring-quartz/src/main/java/ua/in/sz/spring/SampleJob.java
@@ -3,7 +3,6 @@ package ua.in.sz.spring;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -11,7 +10,7 @@ import org.springframework.stereotype.Component;
 public class SampleJob implements Job {
 
 	@Override
-	public void execute(JobExecutionContext context) throws JobExecutionException {
+	public void execute(JobExecutionContext context) {
 		log.info("Execute");
 	}
 }
