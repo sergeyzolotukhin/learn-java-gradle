098d3ccf549404f46c58641e27ca6b2a4119992a
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-02 21:32:19 +0300

    SQL thread

--- spring-hibernate-thread/src/main/java/ua/in/sz/spring/thread/ApplicationCommandLineRunner.java
+++ spring-hibernate-thread/src/main/java/ua/in/sz/spring/thread/ApplicationCommandLineRunner.java
@@ -2,6 +2,7 @@ package ua.in.sz.spring.thread;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,12 @@ import ua.in.sz.spring.thread.entity.ScheduleEntity;
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Slf4j
 @Getter
@@ -20,9 +27,32 @@ public class ApplicationCommandLineRunner implements CommandLineRunner {
 	private final EntityManager entityManager;
 
 	@Override
+	@SneakyThrows
 	public void run(String...args) {
+		log.info("Start application");
+
+		ExecutorService executor = Executors.newFixedThreadPool(10);
+
+		List<Callable<Void>> tasks = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			tasks.add(this::runQuery);
+		}
+
+		List<Future<Void>> futures = executor.invokeAll(tasks);
+
+		for (Future<Void> future : futures) {
+			future.get();
+		}
+
+		executor.shutdown();
+
+		log.info("End application");
+	}
+
+	private Void runQuery() {
 		long count = countEntity();
 		log.info("Entity count {}", count);
+		return null;
 	}
 
 	private long countEntity() {
