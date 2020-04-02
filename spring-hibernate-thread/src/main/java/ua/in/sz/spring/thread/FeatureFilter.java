2746e2e019565291403ab32674246196adea1090
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-03 00:26:43 +0300

    SQL thread

---
+++ spring-hibernate-thread/src/main/java/ua/in/sz/spring/thread/FeatureFilter.java
@@ -0,0 +1,34 @@
+package ua.in.sz.spring.thread;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.turbo.MatchingFilter;
+import ch.qos.logback.core.spi.FilterReply;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.slf4j.Marker;
+
+@Slf4j
+@Setter
+public class FeatureFilter extends MatchingFilter {
+	String MDCKey;
+	String value;
+
+	public FilterReply decide(Marker marker, Logger logger, Level level, String format, Object[] params, Throwable t) {
+		if (MDCKey == null) {
+			return FilterReply.NEUTRAL;
+		}
+
+		String value = MDC.get(MDCKey);
+		if (value == null) {
+			return FilterReply.NEUTRAL;
+		}
+
+		if (this.value.equals(value)) {
+			return onMatch;
+		} else {
+			return onMismatch;
+		}
+	}
+}
