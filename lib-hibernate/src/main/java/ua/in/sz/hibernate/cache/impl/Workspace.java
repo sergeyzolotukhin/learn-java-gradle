3d6c3b8d713567aecde335be444a8e57e0e130b5
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-06 22:15:58 +0300

    Antlr Visitor

--- lib-hibernate/src/main/java/ua/in/sz/hibernate/cache/impl/Workspace.java
+++ lib-hibernate/src/main/java/ua/in/sz/hibernate/cache/impl/Workspace.java
@@ -8,15 +8,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldNameConstants;
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.Collections;
