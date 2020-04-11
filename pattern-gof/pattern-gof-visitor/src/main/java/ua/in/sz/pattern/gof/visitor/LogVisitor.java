229d79f057f215bc6548d2e18d7db89f7c14e916
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-12 00:02:05 +0300

    GOF Composite

--- pattern-gof/pattern-gof-visitor/src/main/java/ua/in/sz/pattern/gof/visitor/LogVisitor.java
+++ pattern-gof/pattern-gof-visitor/src/main/java/ua/in/sz/pattern/gof/visitor/LogVisitor.java
@@ -1,7 +1,9 @@
-package ua.in.sz.pattern.gof.visitor.impl;
+package ua.in.sz.pattern.gof.visitor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import ua.in.sz.pattern.gof.visitor.impl.Component;
+import ua.in.sz.pattern.gof.visitor.impl.Visitor;
 
 @Slf4j
 public class LogVisitor implements Visitor {
