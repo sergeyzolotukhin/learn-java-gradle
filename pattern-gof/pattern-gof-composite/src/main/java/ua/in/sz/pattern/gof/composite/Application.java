c0c66a13a003b4d7d18677afa60b7b817524fd35
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-11 23:18:54 +0300

    GOF Composite

--- pattern-gof/pattern-gof-composite/src/main/java/ua/in/sz/pattern/gof/composite/Application.java
+++ pattern-gof/pattern-gof-composite/src/main/java/ua/in/sz/pattern/gof/composite/Application.java
@@ -8,8 +8,13 @@ import ua.in.sz.pattern.gof.composite.impl.Leaf;
 public class Application {
 	public static void main(String[] args) {
 		Composite root = Composite.builder().name("root").build();
-		root.add(Leaf.builder().name("child 1").build());
+		Composite child1 = Composite.builder().name("child 1").build();
+
+		root.add(child1);
 		root.add(Leaf.builder().name("child 2").build());
+
+		child1.add(Leaf.builder().name("child 1-1").build());
+		child1.add(Leaf.builder().name("child 1-2").build());
 
 		root.draw();
 	}
