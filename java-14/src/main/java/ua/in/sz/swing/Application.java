55136aa4e86603643890eb4bd860b98f6b0cb74d
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-11 06:52:43 +0300

    JDK 14

--- java-14/src/main/java/ua/in/sz/swing/Application.java
+++ java-14/src/main/java/ua/in/sz/swing/Application.java
@@ -7,15 +7,47 @@ public class Application {
 	public static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
-		Object object = "Serhij";
 
-		cast(object);
+		int n = 7;
+		int no = 0;
+//		no = switchExpression(n);
+		no = switchReturn(n);
+
+
+		doSwitch(no);
+//		cast("Serhij");
 	}
 
-	private static void cast(Object obj) {
+	private static int switchReturn(int n) {
+		return switch (n) {
+			case 7,15 -> 2;
+			default -> {
+				log.info("Default run");
+				yield 7;
+			}
+		};
+	}
+
+	private static int switchExpression(int n) {
+		int no = switch (n) {
+			case 7,15 -> 2;
+			default -> 1;
+		};
+
+		return no;
+	}
+
+	private static void doSwitch(int no) {
+		switch (no) {
+			case 1,2 -> log.info("One or Two");
+			default -> log.info("Other");
+		}
+	}
+
+/*	private static void cast(Object obj) {
 		if (obj instanceof String s) {
 			String substring = s.substring(2);
 			log.info("Text: {}", substring);
 		}
-	}
+	}*/
 }
