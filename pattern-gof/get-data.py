d586e499051222de72a1684e9bb2d9173ba20415
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-18 03:10:14 +0300

    Python automation tasks

--- pattern-gof/get-data.py
+++ pattern-gof/get-data.py
@@ -1,6 +1,25 @@
 import subprocess
+import os
+from pathlib import Path
+import shutil
+
+dirPath = Path('.', 'build', 'gof-a-v0.0.1')
+if dirPath.exists() and dirPath.is_dir():
+    shutil.rmtree(dirPath)
+print("Target dir was removed")
+
+zipPath = Path('.', 'build', 'gof-a-v0.0.1.zip')
+if zipPath.exists() and zipPath.is_fifo():
+    shutil.rmtree(zipPath)
+print("Target zip was removed")
 
 archive = subprocess.run(["git", "archive", "--output=./build/gof-a-v0.0.1.zip", "v0.0.1", "./pattern-gof-adapter"])
-print("Git archive was created, Status code: %d" % archive.returncode)
+print("Git archive was created")
 
+unzip = subprocess.run(["7z", "x", "build/gof-a-v0.0.1.zip", "-obuild//gof-a-v0.0.1"], capture_output=True)
+print("Data was unarchived")
 
+zipPath = Path('.', 'build', 'gof-a-v0.0.1.zip')
+if zipPath.exists() and zipPath.is_fifo():
+    shutil.rmtree(zipPath)
+print("Target zip was removed")
