0536252e55a5b7d663a371ed7e59ced86e40478a
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-18 05:13:21 +0300

    Python automation tasks

--- pattern-gof/get-data.py
+++ pattern-gof/get-data.py
@@ -3,30 +3,31 @@ import logging as log
 from pathlib import Path
 import shutil
 import sys
+import os
 
 FORMAT = '%(asctime)23.23s [ %(levelname)10.10s ] %(filename)20.20s:%(lineno)4.4s - %(message)s'
-log.basicConfig(stream=sys.stdout, level=log.INFO, format=FORMAT)
+log.basicConfig(stream=sys.stdout, level=log.DEBUG, format=FORMAT)
 
 version = 'v0.0.1'
 baseName = 'gof-a'
 archiveExtension = 'zip'
 dirName = f'{baseName}-{version}'
 archiveName = f'{dirName}.{archiveExtension}'
+moduleDir = 'pattern-gof'
 
 dirPath = Path('.', 'build', f'{dirName}')
 if dirPath.exists() and dirPath.is_dir():
     shutil.rmtree(dirPath)
 
-zipPath = Path('.', 'build', f'{archiveName}')
-if zipPath.exists() and zipPath.is_fifo():
-    shutil.rmtree(zipPath)
+zipPath = Path('.', 'build', archiveName)
+if zipPath.exists() and zipPath.is_file():
+    os.remove(zipPath)
 
 archive = subprocess.run(["git", "archive", f'--output=./build/{archiveName}', f'{version}', "./pattern-gof-adapter"])
 
 unzip = subprocess.run(["7z", "x", f'build/{archiveName}', f'-obuild//{dirName}'], capture_output=True)
 
-zipPath = Path('.', 'build', f'{archiveName}')
-if zipPath.exists() and zipPath.is_fifo():
-    shutil.rmtree(zipPath)
+if zipPath.exists() and zipPath.is_file():
+    os.remove(zipPath)
 
 log.info(f'Checkout data from git with tag "{version}" to path: [./build/{dirName}/pattern-gof-adapter]')
