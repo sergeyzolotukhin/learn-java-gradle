88d82268953f47005da2a7354f4424fd230bfc60
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-22 08:19:32 +0300

    Spring datasource initialization

--- spring-web-xml/src/main/java/ua/in/sz/spring/HelloWorldController.java
+++ spring-web-xml/src/main/java/ua/in/sz/spring/HelloWorldController.java
@@ -1,46 +1,70 @@
 package ua.in.sz.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.concurrent.TimeUnit;
 
 public class HelloWorldController extends AbstractController {
 
-	@Autowired
-	private TaskExecutor taskExecutor;
+    private TaskExecutor taskExecutor;
+    private DataSource dataSource;
 
-	public void setTaskExecutor(TaskExecutor taskExecutor) {
-		this.taskExecutor = taskExecutor;
-	}
+    public void setTaskExecutor(TaskExecutor taskExecutor) {
+        this.taskExecutor = taskExecutor;
+    }
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
-		logger.info("Get hello request");
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+                                                 HttpServletResponse response) throws Exception {
 
-		ModelAndView model = new ModelAndView("HelloWorldPage");
-		model.addObject("msg", "hello world");
+        logger.info("Get hello request");
 
-		taskExecutor.execute(this::doTask);
+        ModelAndView model = new ModelAndView("HelloWorldPage");
+        model.addObject("msg", "hello world");
 
-		return model;
-	}
+        taskExecutor.execute(this::doTask);
 
-	private void doTask() {
-		try {
-			logger.info("Start task");
-			TimeUnit.SECONDS.sleep(20);
-			logger.info("End task");
-		} catch (Exception e) {
-			logger.error("Can't do task", e);
-		}
+        return model;
+    }
 
-	}
+    private void doTask() {
+        try {
+            logger.info("Start task");
+            TimeUnit.SECONDS.sleep(20);
+
+            long count = countEmployee();
+            logger.info(String.format("Employee count: %d", count));
+
+            logger.info("End task");
+        } catch (Exception e) {
+            logger.error("Can't do task", e);
+        }
+
+    }
+
+    private long countEmployee() throws SQLException {
+        try (
+                Connection connection = dataSource.getConnection();
+                Statement statement = connection.createStatement();
+
+                ResultSet resultSet = statement.executeQuery("select count(*) from employee");
+        ) {
+            resultSet.next();
+            return resultSet.getLong(1);
+        }
+    }
 
 }
