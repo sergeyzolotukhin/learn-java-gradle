cba777b4515b1103b8f3d23149a160c359b966fb
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-22 19:21:42 +0300

    Spring datasource initialization

--- spring-web-xml/src/main/java/ua/in/sz/spring/HelloWorldController.java
+++ spring-web-xml/src/main/java/ua/in/sz/spring/HelloWorldController.java
@@ -11,7 +11,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.concurrent.TimeUnit;
 
 public class HelloWorldController extends AbstractController {
 
