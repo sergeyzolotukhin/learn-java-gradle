1ffd3ea0d00fcdb88e91d95da98dc4cd6065717b
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-22 07:37:40 +0300

    Spring datasource initialization

--- spring-web-xml/src/main/java/ua/in/sz/spring/HelloWorldController.java
+++ spring-web-xml/src/main/java/ua/in/sz/spring/HelloWorldController.java
@@ -1,12 +1,22 @@
 package ua.in.sz.spring;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.TimeUnit;
+
 public class HelloWorldController extends AbstractController {
+
+	@Autowired
+	private TaskExecutor taskExecutor;
+
+	public void setTaskExecutor(TaskExecutor taskExecutor) {
+		this.taskExecutor = taskExecutor;
+	}
 
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
@@ -17,7 +27,20 @@ public class HelloWorldController extends AbstractController {
 		ModelAndView model = new ModelAndView("HelloWorldPage");
 		model.addObject("msg", "hello world");
 
+		taskExecutor.execute(this::doTask);
+
 		return model;
+	}
+
+	private void doTask() {
+		try {
+			logger.info("Start task");
+			TimeUnit.SECONDS.sleep(20);
+			logger.info("End task");
+		} catch (Exception e) {
+			logger.error("Can't do task", e);
+		}
+
 	}
 
 }
