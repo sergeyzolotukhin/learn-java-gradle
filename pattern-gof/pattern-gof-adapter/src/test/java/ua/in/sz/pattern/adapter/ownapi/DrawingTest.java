92d05c092f530881c7ca4115a459217e8f0e2177
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-25 09:49:48 +0300

    Spring datasource TX

---
+++ pattern-gof/pattern-gof-adapter/src/test/java/ua/in/sz/pattern/adapter/ownapi/DrawingTest.java
@@ -0,0 +1,15 @@
+package ua.in.sz.pattern.adapter.ownapi;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class DrawingTest {
+
+    @Test
+    @DisplayName("I can render use own drawing api")
+    void render() {
+        fail("Filed tes");
+    }
+}
