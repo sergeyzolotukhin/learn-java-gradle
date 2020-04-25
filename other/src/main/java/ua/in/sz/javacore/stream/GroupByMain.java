cbf72adb8a8e8caac27512dc35a9e5a0937c84e1
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-25 20:39:24 +0300

    Spring datasource TX

--- other/src/main/java/ua/in/sz/javacore/stream/GroupByMain.java
+++ other/src/main/java/ua/in/sz/javacore/stream/GroupByMain.java
@@ -1,14 +1,9 @@
 package ua.in.sz.javacore.stream;
 
-import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
