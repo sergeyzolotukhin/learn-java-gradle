6b0c47f2acd04346c2d9c2c2e069f5dede8680a4
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-27 19:57:40 +0300

    Logback MDC

---
+++ public-key.sh
@@ -0,0 +1,5 @@
+mkdir /home/szolotukhin/.ssh
+touch /home/szolotukhin/.ssh/authorized_keys
+chown -R szolotukhin:szolotukhin /home/szolotukhin/.ssh
+chmod 700 /home/szolotukhin/.ssh
+chmod 600 /home/szolotukhin/.ssh/authorized_keys
