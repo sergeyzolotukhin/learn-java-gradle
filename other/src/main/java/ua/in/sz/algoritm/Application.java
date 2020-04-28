1674912d39a239b78fed2c66212028e24f765f0b
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-04-28 23:52:29 +0300

    Logback MDC

--- other/src/main/java/ua/in/sz/algoritm/Application.java
+++ other/src/main/java/ua/in/sz/algoritm/Application.java
@@ -3,8 +3,7 @@ package ua.in.sz.algoritm;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Slf4j
 public class Application {
@@ -12,7 +11,16 @@ public class Application {
         List<ValueVO<?>> values1 = Arrays.asList(
                 ValueVO.builder().start(date(0, 0)).end(date(0, 15)).value(1).build(),
                 ValueVO.builder().start(date(0, 15)).end(date(0, 30)).value(2).build(),
-                ValueVO.builder().start(date(0, 30)).end(date(0, 45)).value(3).build()
+                ValueVO.builder().start(date(0, 30)).end(date(0, 45)).value(3).build(),
+                ValueVO.builder().start(date(0, 45)).end(date(1, 0)).value(4).build(),
+                ValueVO.builder().start(date(1, 0)).end(date(1, 15)).value(5).build(),
+                ValueVO.builder().start(date(1, 15)).end(date(1, 30)).value(6).build(),
+                ValueVO.builder().start(date(1, 30)).end(date(1, 45)).value(7).build(),
+                ValueVO.builder().start(date(1, 45)).end(date(2, 0)).value(7).build(),
+                ValueVO.builder().start(date(2, 0)).end(date(2, 15)).value(8).build(),
+                ValueVO.builder().start(date(2, 15)).end(date(2, 30)).value(9).build(),
+                ValueVO.builder().start(date(2, 30)).end(date(2, 45)).value(1).build(),
+                ValueVO.builder().start(date(2, 45)).end(date(3, 0)).value(2).build()
         );
 
         List<ValueVO<?>> values2 = Arrays.asList(
@@ -26,9 +34,31 @@ public class Application {
                 ScheduleVO.builder().start(date(1, 0)).end(date(2, 0)).values(values2).build()
         );
 
-        log.info("{}", schedules);
+        // ============================================================================================================
 
+        ScheduleVO result = ScheduleVO.builder().build();
+        Map<DateTime, ValueVO<?>> valueOnDate = new LinkedHashMap<>();
 
+        for (ScheduleVO schedule : schedules) {
+            result.setStart(min(result.getStart(), schedule.getStart()));
+            result.setStart(max(result.getEnd(), schedule.getEnd()));
+
+            for (ValueVO<?> value : schedule.getValues()) {
+                valueOnDate.put(value.getStart(), value);
+            }
+        }
+
+        result.setValues(new ArrayList<>(valueOnDate.values()));
+
+        log.info("{}", result);
+    }
+
+    private static DateTime min(DateTime d1, DateTime d2){
+        return d1 != null && d1.isBefore(d2) ? d1 : d2;
+    }
+
+    private static DateTime max(DateTime d1, DateTime d2){
+        return d1 != null && d1.isAfter(d2) ? d1 : d2;
     }
 
     private static DateTime date(int h, int m) {
--- other/src/main/java/ua/in/sz/algoritm/ScheduleVO.java
+++ other/src/main/java/ua/in/sz/algoritm/ScheduleVO.java
@@ -2,6 +2,7 @@ package ua.in.sz.algoritm;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
