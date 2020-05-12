f4c339f00396712d8cc7274716038d946287c4ad
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-05-12 22:00:55 +0300

    Create value clusters

--- hibernate/hibernate-xml-mapping/src/main/java/ua/in/sz/hibernate/xml/ScheduleValueClusterApplication.java
+++ hibernate/hibernate-xml-mapping/src/main/java/ua/in/sz/hibernate/xml/ScheduleValueClusterApplication.java
@@ -10,6 +10,7 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ua.in.sz.hibernate.xml.impl.NumberScheduleValue;
 import ua.in.sz.hibernate.xml.impl.Schedule;
+import ua.in.sz.hibernate.xml.impl.StringScheduleValue;
 
 import java.util.Collections;
 import java.util.List;
@@ -40,6 +41,7 @@ public class ScheduleValueClusterApplication {
     private static void findSchedules(SessionFactory sessionFactory) {
         doInStatelessSession(sessionFactory, session -> {
             log.info("Loading schedules");
+            Stopwatch stopwatch = Stopwatch.createStarted();
 
             List<Long> workspaceIds = findWorkspaceIds(session, Collections.singletonList("2020-01-01"));
 
@@ -47,60 +49,72 @@ public class ScheduleValueClusterApplication {
 
             List<Long> scheduleIds = schedules.stream().map(Schedule::getId).collect(Collectors.toList());
 
-            List<NumberScheduleValue> numberScheduleValues = findNumberScheduleValues(session, scheduleIds);
+            List<NumberScheduleValue> numberValues = findNumberScheduleValues(session, scheduleIds);
+            List<StringScheduleValue> stringValues = findStringScheduleValues(session, scheduleIds);
 
-            log.info("Loaded schedules, count {}", schedules.size());
+            log.info("Loaded schedules, count {}, numbers {}, strings {}, time {}",
+                    schedules.size(), numberValues.size(), stringValues.size(),
+                    stopwatch.stop());
 
             return Collections.emptyList();
         });
     }
 
     private static List<Long> findWorkspaceIds(StatelessSession session, List<String> names) {
-        log.info("Find workspace ids");
+        log.trace("Find workspace ids");
         Stopwatch stopwatch = Stopwatch.createStarted();
 
         List<Long> result = session.createQuery(
-                "select w.id " +
-                        "from Workspace w " +
-                        "where w.name in :names", Long.class)
+                "select w.id from Workspace w where w.name in :names", Long.class)
                 .setParameterList("names", names)
                 .list();
 
-        log.info("Found workspaces. count: {}, time: {}", CollectionUtils.size(result), stopwatch.stop());
+        log.trace("Found workspaces. count: {}, time: {}", CollectionUtils.size(result), stopwatch.stop());
 
         return result;
     }
 
     private static List<Schedule> findSchedules(StatelessSession session, List<Long> workspaceIds) {
-        log.info("Find schedules");
+        log.trace("Find schedules");
         Stopwatch stopwatch = Stopwatch.createStarted();
 
         List<Schedule> result = session.createQuery(
-                "select s " +
-                        "from Schedule s " +
-                        "where s.workspace.id in (:workspaceIds)"
+                "select s from Schedule s where s.workspace.id in (:workspaceIds)"
                 , Schedule.class)
                 .setParameterList("workspaceIds", workspaceIds)
                 .list();
 
-        log.info("Found schedules. count: {}, time: {}", CollectionUtils.size(result), stopwatch.stop());
+        log.trace("Found schedules. count: {}, time: {}", CollectionUtils.size(result), stopwatch.stop());
 
         return result;
     }
 
     private static List<NumberScheduleValue> findNumberScheduleValues(StatelessSession session, List<Long> scheduleIds) {
-        log.info("Find number schedule values");
+        log.trace("Find number schedule values");
         Stopwatch stopwatch = Stopwatch.createStarted();
 
         List<NumberScheduleValue> result = session.createQuery(
-                "select n " +
-                        "from NumberScheduleValue n " +
-                        "where n.schedule.id in (:scheduleIds)"
+                "select n from NumberScheduleValue n where n.schedule.id in (:scheduleIds)"
                 , NumberScheduleValue.class)
                 .setParameterList("scheduleIds", scheduleIds)
                 .list();
 
-        log.info("Found number schedule values. count: {}, time: {}", CollectionUtils.size(result), stopwatch.stop());
+        log.trace("Found number schedule values. count: {}, time: {}", CollectionUtils.size(result), stopwatch.stop());
+
+        return result;
+    }
+
+    private static List<StringScheduleValue> findStringScheduleValues(StatelessSession session, List<Long> scheduleIds) {
+        log.trace("Find string schedule values");
+        Stopwatch stopwatch = Stopwatch.createStarted();
+
+        List<StringScheduleValue> result = session.createQuery(
+                "select n from StringScheduleValue n where n.schedule.id in (:scheduleIds)"
+                , StringScheduleValue.class)
+                .setParameterList("scheduleIds", scheduleIds)
+                .list();
+
+        log.trace("Found string schedule values. count: {}, time: {}", CollectionUtils.size(result), stopwatch.stop());
 
         return result;
     }
