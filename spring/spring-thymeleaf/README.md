ff71eaa5c00c725f955533eb4a36021981a8318d
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-05-21 17:19:08 +0300

    spring thymeleaf

--- spring/spring-thymeleaf/README.md
+++ spring/spring-thymeleaf/README.md
@@ -1,5 +1,3 @@
 # spring_shell
 
-https://medium.com/agency04/developing-cli-application-with-spring-shell-part-1-807cd3a32461
-https://medium.com/agency04/developing-cli-application-with-spring-shell-part-3-b4c247fdf558
-
+https://medium.com/@bhanuchaddha/spring-boot-devtools-on-intellij-c0ab3f9afa63
--- spring/spring-thymeleaf/src/main/java/ua/in/sz/shell/shell/command/MainController.java
+++ spring/spring-thymeleaf/src/main/java/ua/in/sz/shell/shell/command/MainController.java
@@ -1,15 +1,16 @@
 package ua.in.sz.shell.shell.command;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 public class MainController {
     @GetMapping("/index")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", "sz");
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
+        log.info("Get faster");
         return "index";
     }
 }
