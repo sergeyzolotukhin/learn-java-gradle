4e8c341c728bacdef6d1f78cd2f3d8fb8325b8cd
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-05-21 17:00:19 +0300

    spring thymeleaf

--- spring/spring-thymeleaf/src/main/java/ua/in/sz/shell/shell/command/MainController.java
+++ spring/spring-thymeleaf/src/main/java/ua/in/sz/shell/shell/command/MainController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @GetMapping("/index")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+        model.addAttribute("name", "sz");
         return "index";
     }
 }
