00be232d6fb4e12bd708af1f3482dba598fd8e8b
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-05-21 16:06:54 +0300

    spring thymeleaf

---
+++ spring/spring-thymeleaf/src/main/java/ua/in/sz/shell/shell/Application.java
@@ -0,0 +1,15 @@
+package ua.in.sz.shell.shell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setLogStartupInfo(false);
+        app.run(args);
+    }
+
+}
