260995f033c8ff5dc102d2a25f9aa2b34e027eb7
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-07-16 02:36:05 +0300

    zoo keeper

--- other/src/main/java/ua/in/sz/dsl/lambda/Main.java
+++ other/src/main/java/ua/in/sz/dsl/lambda/Main.java
@@ -12,13 +12,19 @@ public class Main {
     public static void main(String[] args) {
         DateTime day = DateTime.now();
 
-        Schedule schedule = Schedules.dailyFiveMinutes("S", day)
+        Schedule scheduleA = Schedules.dailyFiveMinutes("S", day)
                 .params(Params.param("A", "RO1"))
                 .values(Values.values("A", "0", "1", "2"),
                         Values.values("B", "0", "1", "2"))
                 .build();
 
-        String value1 = schedule.getValue("A", day.withTimeAtStartOfDay().withMinuteOfHour(5));
+        Schedule scheduleB = Schedules.dailyFiveMinutes("S", day)
+                .params(Params.param("A", "RO2"))
+                .values(Values.values("A", "0", "1", "2"),
+                        Values.values("B", "0", "1", "2"))
+                .build();
+
+        String value1 = scheduleA.getValue("A", day.withTimeAtStartOfDay().withMinuteOfHour(5));
         log.info("The value of [{}] is [{}]", "A", value1);
     }
 }
