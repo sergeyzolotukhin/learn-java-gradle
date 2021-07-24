8ae5b6d29ee81c587a54bbfa3c9fb77532c9e363
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-07-24 03:45:02 +0300

    zoo keeper

--- other/src/main/java/ua/in/sz/dsl/lambda/Main.java
+++ other/src/main/java/ua/in/sz/dsl/lambda/Main.java
@@ -2,32 +2,30 @@ package ua.in.sz.dsl.lambda;
 
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
+import ua.in.sz.dsl.domain.Types;
 import ua.in.sz.dsl.model.Schedule;
 import ua.in.sz.dsl.model.factories.Params;
 import ua.in.sz.dsl.model.factories.Schedules;
 import ua.in.sz.dsl.model.factories.Values;
-
-import static ua.in.sz.dsl.domain.Types.AVAILABILITY;
-import static ua.in.sz.dsl.domain.Types.FORECAST;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
         DateTime day = DateTime.now();
 
-        Schedule availability = Schedules.dailyFiveMinutes(AVAILABILITY, day)
-                .params(Params.param(AVAILABILITY.RESOURCE, "RO1"))
-                .values(Values.values(AVAILABILITY.QUANTITY, "0", "1", "2"),
-                        Values.values(AVAILABILITY.PRICE, "0", "1", "2"))
+        Schedule availability = Schedules.dailyFiveMinutes(Types.AVAILABILITY, day)
+                .params(Params.param(Types.AVAILABILITY.RESOURCE, "RO1"))
+                .values(Values.values(Types.AVAILABILITY.QUANTITY, "0", "1", "2"),
+                        Values.values(Types.AVAILABILITY.PRICE, "0", "1", "2"))
                 .build();
 
-        Schedule forecast = Schedules.dailyFiveMinutes(FORECAST, day)
-                .params(Params.param(FORECAST.RESOURCE, "RO2"))
-                .values(Values.values(FORECAST.VALUE, "0", "1", "2"),
-                        Values.values(FORECAST.PRICE, "0", "1", "2"))
+        Schedule forecast = Schedules.dailyFiveMinutes(Types.FORECAST, day)
+                .params(Params.param(Types.FORECAST.RESOURCE, "RO2"))
+                .values(Values.values(Types.FORECAST.VALUE, "0", "1", "2"),
+                        Values.values(Types.FORECAST.PRICE, "0", "1", "2"))
                 .build();
 
-        String value1 = availability.getValue(AVAILABILITY.QUANTITY, day.withTimeAtStartOfDay().withMinuteOfHour(5));
+        String value1 = availability.getValue(Types.AVAILABILITY.QUANTITY, day.withTimeAtStartOfDay().withMinuteOfHour(5));
         log.info("The value of [{}] is [{}]", "A", value1);
     }
 }
