e755cbaf063b31ff8867663e956250ef78c4c37b
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-07-25 22:29:28 +0300

    zoo keeper

---
+++ house/src/main/java/ua/in/sz/house/Heating.java
@@ -0,0 +1,9 @@
+package ua.in.sz.house;
+
+public class Heating {
+    public static void main(String[] args) {
+        GasHeating.main(null);
+        ElectricityHeating.main(null);
+        SolidFuelHeating.main(null);
+    }
+}
