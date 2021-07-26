21c713dcaba38a78ecdd61c74e82feb45a60272c
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-07-26 22:44:57 +0300

    zoo keeper

--- house/src/main/java/ua/in/sz/house/House.java
+++ house/src/main/java/ua/in/sz/house/House.java
@@ -2,10 +2,8 @@ package ua.in.sz.house;
 
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
-import ua.in.sz.house.model.Block;
 import ua.in.sz.house.model.Wall;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -25,21 +23,5 @@ public class House {
                 .sum();
     }
 
-    public static void main(String[] args) {
-        Block block = Block.CERAMICS_BRICK;
-        int height = 3;
 
-        House house = House.builder()
-                .walls(Arrays.asList(
-                        Wall.builder().block(block).length(10).height(height).build(),
-                        Wall.builder().block(block).length(10).height(height).build(),
-                        Wall.builder().block(block).length(10).height(height).build(),
-                        Wall.builder().block(block).length(10).height(height).build()
-                ))
-                .build();
-
-        double heatLossKWt = house.getHeatLoss(20.0, -20.0) / 1000.0;
-        log.info("Heat loss is {} KWt on wall square {} M2", String.format("%.2f", heatLossKWt),
-                house.getWallSquare());
-    }
 }
---
+++ house/src/main/java/ua/in/sz/house/Main.java
@@ -0,0 +1,32 @@
+package ua.in.sz.house;
+
+import lombok.extern.slf4j.Slf4j;
+import ua.in.sz.house.model.Block;
+import ua.in.sz.house.model.Wall;
+
+import java.util.Arrays;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        House house = buildHouse();
+
+        double heatLossKWt = house.getHeatLoss(20.0, -20.0) / 1000.0;
+        log.info("Heat loss is {} KWt on wall square {} M2", String.format("%.2f", heatLossKWt),
+                house.getWallSquare());
+    }
+
+    private static House buildHouse() {
+        Block block = Block.CERAMICS_BRICK;
+        int height = 3;
+
+        return House.builder()
+                .walls(Arrays.asList(
+                        Wall.builder().block(block).length(10).height(height).build(),
+                        Wall.builder().block(block).length(10).height(height).build(),
+                        Wall.builder().block(block).length(10).height(height).build(),
+                        Wall.builder().block(block).length(10).height(height).build()
+                ))
+                .build();
+    }
+}
