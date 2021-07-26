b596044e52bb8b181f6784664587a63301c049c9
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-07-26 21:46:30 +0300

    zoo keeper

--- house/src/main/java/ua/in/sz/house/BrickHouse.java
+++ house/src/main/java/ua/in/sz/house/BrickHouse.java
@@ -1,5 +1,7 @@
 package ua.in.sz.house;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,20 +19,35 @@ public class BrickHouse {
     }
 
     private double wallSquare() {
-        return (10.0 + 10.0 + 10.0 + 10.0) * 2.75;
+        return 10.0 * 7.0 * 4.0;
     }
 
     private double heatTransferRatio() {
-        double wallThickness = 0.120 * 2.0 + 0.010;
-        double brickHeatTransfer = 0.7; // Вт/м * К
-        double aIn = 8.7;
-        double aOut = 23.0;
-        return 1.0 / (1.0 / aIn + wallThickness / brickHeatTransfer + 1.0 / aOut);
+        int nBrick = 2;
+//        double wallThickness = 0.250 * nBrick + 0.010 * (nBrick - 1); // M
+        double wallThickness = 0.5; // M
+        double brickHeatTransfer = HeatTransfer.CERAMICS.getRatio(); // Вт/м * C
+        double rWall = wallThickness / brickHeatTransfer;
+
+        return 1.0 / rWall;
     }
 
     public static void main(String[] args) {
         BrickHouse house = new BrickHouse();
-        double heatLoss = house.heatLoss(23.0, 15.0);
-        log.info("Heat loss is {} KWt", heatLoss / 1000.0);
+        double heatLoss = house.heatLoss(20.0, -20.0);
+        log.info("Heat loss is {} KWt on wall square {} M2", heatLoss / 1000.0,
+                house.wallSquare());
+    }
+
+    @Getter
+    @AllArgsConstructor
+    private enum HeatTransfer {
+        /**
+         * Кирпичь полнотелый
+         */
+        BRICK(0.7),
+        CERAMICS(0.16)
+        ;
+        private final double ratio;
     }
 }
