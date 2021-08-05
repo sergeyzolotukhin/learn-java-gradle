1c6235fe28649d258ea3d8b25a8500c7fbe57ac2
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-08-05 20:49:50 +0300

    Wall

--- house/src/main/java/ua/in/sz/house/Main.java
+++ house/src/main/java/ua/in/sz/house/Main.java
@@ -17,39 +17,39 @@ public class Main {
 
         House house = House.of(Block.CERAMICS_BRICK);
 
-        Heating electricityHeating = ElectricityHeating.of(house, calendar);
+        Heating heating = ElectricityHeating.of(house, calendar);
         log.info(String.format("Wall width %.0f. Heat loss is %.2f KWt on wall square %.0f M2. " +
                         "Heating cost: %.0f by Electricity",
                 house.getWallWidth() * 1000,
                 house.getHeatLoss(24.0, -20.0) / 1000.0,
                 house.getWallSquare(),
-                electricityHeating.costPerYear() / 12.0
+                heating.costPerYear() / 12.0
         ));
 
-        Heating solidFuelHeating = SolidFuelHeating.of(house, calendar);
+        heating = SolidFuelHeating.of(house, calendar);
         log.info(String.format("Wall width %.0f. Heat loss is %.2f KWt on wall square %.0f M2. " +
                         "Heating cost: %.0f by Solid Fuel",
                 house.getWallWidth() * 1000,
                 house.getHeatLoss(24.0, -20.0) / 1000.0,
                 house.getWallSquare(),
-                solidFuelHeating.costPerYear() / 12.0
+                heating.costPerYear() / 12.0
         ));
 
-        Heating gasHeating = GasHeating.of(house, calendar);
+        heating = GasHeating.of(house, calendar);
         log.info(String.format("Wall width %.0f. Heat loss is %.2f KWt on wall square %.0f M2. " +
                         "Heating cost: %.0f by Gas",
                 house.getWallWidth() * 1000,
                 house.getHeatLoss(24.0, -20.0) / 1000.0,
                 house.getWallSquare(),
-                gasHeating.costPerYear() / 6.0
+                heating.costPerYear() / 6.0
         ));
 
 
         // GAS CONCRETE
         house = House.of(Block.GAS_CONCRETE_BLOCK_D500);
-        electricityHeating = ElectricityHeating.of(house, calendar);
-        solidFuelHeating = SolidFuelHeating.of(house, calendar);
-        gasHeating = GasHeating.of(house, calendar);
+        Heating electricityHeating = ElectricityHeating.of(house, calendar);
+        Heating solidFuelHeating = SolidFuelHeating.of(house, calendar);
+        Heating gasHeating = GasHeating.of(house, calendar);
 
         log.info(String.format("Wall width %.0f. Heat loss is %.2f KWt on wall square %.0f M2. " +
                         "Heating cost: %.0f by Electricity, %.0f by Solid Fuel, %.0f by Gas",
