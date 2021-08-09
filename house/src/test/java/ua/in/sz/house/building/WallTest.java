a129e0423c47d09e4cb47356c05983e02c7b9d17
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-08-09 22:56:21 +0300

    house

--- house/src/test/java/ua/in/sz/house/building/WallTest.java
+++ house/src/test/java/ua/in/sz/house/building/WallTest.java
@@ -1,6 +1,7 @@
 package ua.in.sz.house.building;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,25 +11,13 @@ class WallTest {
 
     @Test
     void cementMortar() {
-        Block block = Block.CERAMICS_BRICK;
+        Wall wall = Wall.builder().block(Block.CERAMICS_BRICK).height(1.0).length(1.0).build();
 
-        Wall wall = Wall.builder()
-                .block(block)
-                .height(1.0)
-                .length(1.0)
-                .build();
+        double expected = 0.106;
+        double actual = wall.cementMortar();
 
-        double square = wall.getSquare();
-        double v = wall.getWidth() + wall.getLength() * wall.getWidth();
-        double e = v * 0.245; // Расход раствора (кубометр) для кладки в два керамический кирпича
+        log.trace(String.format("Expected %.3f M3 actual %.3f", expected, actual));
 
-        double vb = block.getLength() * block.getHeight() * block.getWidth();
-        double vab = vb * wall.blockCount();
-
-        double cementMortar = wall.cementMortar();
-        log.info(String.format("Cement mortar %.2f", v - vab));
-        log.info(String.format("Cement mortar: %.2f M3 expected %.2f M3", cementMortar, e));
-
-        log.info(String.format("square %.2f M2 width %.2f M block count %.0f", square, wall.getWidth(), wall.blockCount()));
+        Assertions.assertEquals(expected, actual, 0.001);
     }
 }
