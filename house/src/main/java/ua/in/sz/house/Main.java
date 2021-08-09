da702c87768a6f9601cb066a21b897eb8baebe1d
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-08-09 23:26:49 +0300

    house

--- house/src/main/java/ua/in/sz/house/Main.java
+++ house/src/main/java/ua/in/sz/house/Main.java
@@ -26,13 +26,30 @@ public class Main {
                 String.format("\n\twall width %.0f mm", house.getWallWidth() * 1000.0) +
                 String.format("\n\twall heat less %.2f kWt", house.getHeatLoss(TARGET_TEMPERATURE, -20.0) / 1000.0);
 
+
+        double cementMortar = house.cementMortar();
+        double cement = cementKg(cementMortar);
+        double sang = sandKg(cementMortar);
+
         String materialInfo = "\nHouse materials:" +
                 String.format("\n\tblock count %.0f cost %.0f UAH", house.blockCount(), house.blockCount() * 3.3) +
-                String.format("\n\tcement mortar %.2f M3", house.cementMortar());
+                String.format("\n\tcement mortar %.2f M3", cementMortar) +
+                String.format("\n\tcement %.3f T cost %.2f UAH", cement / 1000.0, cement / 1000.0 * 1800.0)  +
+                String.format("\n\tsang %.3f T cost %.2f UAH", sang / 1000.0, sang / 1000.0 * 180.0);
 
         String supportCostInfo = "\nHouse support cost:" +
                 String.format("\n\theating cost per month %.0f UAH", costCalculator.costPerYear() / 12.0);
 
         log.info(houseInfo + materialInfo + supportCostInfo);
+    }
+
+    private static double cementKg(double cementMortar) {
+        double volume = cementMortar / 4.0;
+        return volume * 1625; // кг в м3 песка
+    }
+
+    private static double sandKg(double cementMortar) {
+        double volume = cementMortar * 3.0 / 4.0;
+        return volume * 1500; // кг в м3 цемента
     }
 }
