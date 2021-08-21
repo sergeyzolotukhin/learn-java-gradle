9d57fc6535e95bd0ca236c16e0c4cf007eca331c
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-08-22 02:28:59 +0300

    house~~~~

--- house/src/main/java/ua/in/sz/house/cost/MaterialCostCalculator.java
+++ house/src/main/java/ua/in/sz/house/cost/MaterialCostCalculator.java
@@ -1,7 +1,6 @@
 package ua.in.sz.house.cost;
 
 import com.google.common.collect.ImmutableMap;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ua.in.sz.house.material.Material;
 
