60df39c29b5f126370db5996cb6bce5aae11c194
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-08-28 21:53:00 +0300

    JAVA 16

--- house/src/main/java/ua/in/sz/house/material/MaterialReport.java
+++ house/src/main/java/ua/in/sz/house/material/MaterialReport.java
@@ -1,13 +1,17 @@
 package ua.in.sz.house.material;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class MaterialReport {
     public static String report(Material material) {
         StringBuilder sb = new StringBuilder();
 
         sb.append("\nMaterials:");
+        sb.append(String.format("\n\t%-15.15s %7.7s %7.7s", "name", "quantity", "unit"));
+        sb.append(StringUtils.rightPad("\n\t=", 33, "="));
 
         for (Material component : material.allComponents()) {
-            sb.append(String.format("\n\t%-15.15s quantity %7.0f %7.7s",
+            sb.append(String.format("\n\t%-15.15s %7.0f %7.7s",
                     component.getMaterialType().getName(), component.getQuantity(), component.getUnit().getName()));
         }
 
