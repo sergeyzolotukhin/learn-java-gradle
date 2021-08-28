7646714f33bb00f05a881331bfdd4a36a6646ba5
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-08-28 21:14:57 +0300

    JAVA 16

--- house/src/main/java/ua/in/sz/house/material/MaterialType.java
+++ house/src/main/java/ua/in/sz/house/material/MaterialType.java
@@ -1,9 +1,16 @@
 package ua.in.sz.house.material;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MaterialType {
-    BRICK,
-    CEMENT,
-    CEMENT_MORTAR,
-    SANG,
-    WALL;
+    BRICK("Brick"),
+    CEMENT("Cement"),
+    CEMENT_MORTAR("Cement mortar"),
+    SANG("Sang"),
+    WALL("Wall");
+
+    private final String name;
 }
--- house/src/main/java/ua/in/sz/house/shop/order/MaterialOrderReport.java
+++ house/src/main/java/ua/in/sz/house/shop/order/MaterialOrderReport.java
@@ -7,11 +7,11 @@ public class MaterialOrderReport {
     public static String report(MaterialOrder materialOrder) {
         StringBuilder sb = new StringBuilder();
 
-        sb.append("\nHouse order:");
+        sb.append("\nMaterial order:");
 
         for (MaterialOrder.Item material : materialOrder.items()) {
-            sb.append(String.format("\n\t%s quantity %.0f cost %.0f UAH",
-                    material.materialType(), material.quantity(), material.cost()));
+            sb.append(String.format("\n\t%-10.10s quantity %7.0f cost %10.0f UAH",
+                    material.materialType().getName(), material.quantity(), material.cost()));
         }
 
         return sb.toString();
