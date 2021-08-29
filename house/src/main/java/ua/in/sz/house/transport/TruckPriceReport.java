5f0affab93f4509e32e2a902f5c594cc80573387
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-08-30 00:32:26 +0300

    JAVA 16

--- house/src/main/java/ua/in/sz/house/transport/TruckPriceReport.java
+++ house/src/main/java/ua/in/sz/house/transport/TruckPriceReport.java
@@ -16,6 +16,7 @@ public class TruckPriceReport {
         sb.append(title);
         sb.append(StringUtils.rightPad("\n\t=", size, "="));
 
+        double totalCost = 0.0;
         for (TruckPrice.Item price : truckPrice.items()) {
             sb.append(String.format(
                     "\n\t%-16.16s %9.0f %7.0f %9.0f %9.0f %10.10s",
@@ -25,9 +26,13 @@ public class TruckPriceReport {
                     kgToT(price.weight()),
                     price.cost(),
                     price.materialType().getName()));
+
+            totalCost += price.cost();
         }
 
         sb.append(StringUtils.rightPad("\n\t=", size, "="));
+        sb.append(String.format("\n\t%-16.16s %9.9s %7.7s %9.9s %9.0f %10.10s",
+                StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY, Math.ceil(totalCost), StringUtils.EMPTY));
 
         return sb.toString();
     }
