b384924d78074c2a9af55b4b743e2c02e9c1fabb
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-09-30 00:01:53 +0300

    Open CSV

--- library/lib-opencsv/src/main/java/ua/in/sz/opencsv/Application.java
+++ library/lib-opencsv/src/main/java/ua/in/sz/opencsv/Application.java
@@ -11,32 +11,40 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 
 @Slf4j
 public class Application {
-	@SneakyThrows
-	public static void main(String[] args) {
-		Path path = Paths.get(ClassLoader.getSystemResource("data.csv").toURI());
-		try (Reader reader = Files.newBufferedReader(path)) {
-			List<Employee> employees = csvToEmployee(reader).parse();
+    @SneakyThrows
+    public static void main(String[] args) {
+        Path path = Paths.get(ClassLoader.getSystemResource("data.csv").toURI());
+        try (Reader reader = Files.newBufferedReader(path)) {
+            List<Employee> employees = csvToEmployee(reader).parse();
 
-			for (Employee employee : employees) {
-				log.info("{}", employee);
+            for (Employee employee : employees) {
+                log.info("{}", employee);
+            }
+        }
+
+        try (Scanner scanner = new Scanner(path)) {
+			while (scanner.hasNext()) {
+				String line = scanner.next();
+				log.info("{}", line);
 			}
 		}
-	}
+    }
 
-	private static CsvToBean<Employee> csvToEmployee(Reader reader) {
-		return new CsvToBeanBuilder<Employee>(reader)
-				.withType(Employee.class)
-				.withMappingStrategy(employeeMappingStrategy())
-				.build();
-	}
+    private static CsvToBean<Employee> csvToEmployee(Reader reader) {
+        return new CsvToBeanBuilder<Employee>(reader)
+                .withType(Employee.class)
+                .withMappingStrategy(employeeMappingStrategy())
+                .build();
+    }
 
-	private static FuzzyMappingStrategy<Employee> employeeMappingStrategy() {
-		FuzzyMappingStrategy<Employee> ms = new FuzzyMappingStrategy<>();
-		ms.setType(Employee.class);
-		return ms;
-	}
+    private static FuzzyMappingStrategy<Employee> employeeMappingStrategy() {
+        FuzzyMappingStrategy<Employee> ms = new FuzzyMappingStrategy<>();
+        ms.setType(Employee.class);
+        return ms;
+    }
 }
