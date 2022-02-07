7e11e63921079a91f2a5cf4bb7f3150bad95037b
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2022-02-07 23:14:48 +0200

    second task

--- infrastructure/jenkins/provision.sh
+++ infrastructure/jenkins/provision.sh
@@ -12,11 +12,11 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FCEF32E745F2C3D5 >
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get -y install default-jdk jenkins > /dev/null 2>&1
 
-echo "Installing Jenkins default user and config"
-sudo cp $VAGRANT_HOST_DIR/JenkinsConfig/config.xml /var/lib/jenkins/
-sudo mkdir -p /var/lib/jenkins/users/admin
-sudo cp $VAGRANT_HOST_DIR/JenkinsConfig/users/admin/config.xml /var/lib/jenkins/users/admin/
-sudo chown -R jenkins:jenkins /var/lib/jenkins/users/
+#echo "Installing Jenkins default user and config"
+#sudo cp $VAGRANT_HOST_DIR/JenkinsConfig/config.xml /var/lib/jenkins/
+#sudo mkdir -p /var/lib/jenkins/users/admin
+#sudo cp $VAGRANT_HOST_DIR/JenkinsConfig/users/admin/config.xml /var/lib/jenkins/users/admin/
+#sudo chown -R jenkins:jenkins /var/lib/jenkins/users/
 
 ########################
 # Node & npm
