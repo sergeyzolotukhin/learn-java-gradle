f6069a5fb90972f0b148ba30e7045f6efaeb9a44
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2022-02-21 21:46:40 +0200

    second task

---
+++ java-core/eclipse-collections/src/main/java/ua/in/sz/Application.java
@@ -0,0 +1,20 @@
+package ua.in.sz;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+@Slf4j
+public class Application {
+    public static void main(String[] args) {
+        BigDecimal val = new BigDecimal("1.1");
+        log.info("{}", val);
+
+        BigInteger i = new BigInteger("123456");
+        log.info("{}", i);
+
+        long numBits = 1;
+        System.out.println(numBits);
+    }
+}
