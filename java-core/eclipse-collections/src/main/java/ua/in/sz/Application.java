34c0948313053a00cfb2d93548bdd31b3c3a93c9
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2022-02-21 21:47:06 +0200

    second task

--- java-core/eclipse-collections/src/main/java/ua/in/sz/Application.java
+++ java-core/eclipse-collections/src/main/java/ua/in/sz/Application.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
+/**
+ * https://www.eclipse.org/collections/
+ */
 @Slf4j
 public class Application {
     public static void main(String[] args) {
