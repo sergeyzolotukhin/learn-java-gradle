4ce3375eb4dc5483bfd72e54cfe5a41d50cecc23
serhij zolotukhin <serhij.zolotukhin@gmail.com>
2022-05-23 21:45:33 +0300

    Postgresql shell

---
+++ infrastructure/postgresql-shell/README.md
@@ -0,0 +1,5 @@
+#### TODO:
+    1. time zone
+    2. encode UTF-8
+    3 # speed up apt-get, config.cache.auto_detect = true
+    4 move plsql to separate file
--- infrastructure/postgresql-shell/install-postgresql.sh
+++ infrastructure/postgresql-shell/install-postgresql.sh
@@ -4,14 +4,11 @@ sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 sudo apt-get update
-sudo apt-get -y install postgresql
+sudo apt-get -y install postgresql-14
+sudo apt-get -y install mc
 
-sudo sed -i "s/#listen_address.*/listen_addresses '*'/" /etc/postgresql/14/main/postgresql.conf
-
-sudo cat >> /etc/postgresql/14/main/pg_hba.conf <<EOF
-  # Accept all IPv4 connections - FOR DEVELOPMENT ONLY!!!
-  host    all         all         0.0.0.0/0             md5
-EOF
+sudo cp /vagrant/postgresql.conf /etc/postgresql/14/main/postgresql.conf
+sudo cp /vagrant/pg_hba.conf /etc/postgresql/14/main/pg_hba.conf
 
 sudo su postgres -c "psql -c \"CREATE ROLE vagrant SUPERUSER LOGIN PASSWORD 'vagrant'\" "
 
