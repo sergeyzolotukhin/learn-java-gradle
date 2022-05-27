2a14914a30e20c5240dbda4ab507337f4620a771
serhij zolotukhin <serhij.zolotukhin@gmail.com>
2022-05-27 14:02:57 +0300

    Postgresql shell

--- infrastructure/postgresql-shell/install-postgresql.sh
+++ infrastructure/postgresql-shell/install-postgresql.sh
@@ -2,16 +2,16 @@
 
 echo 'Add postgresql repository'
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+wget -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 echo 'Update index of packages'
-sudo apt-get -qq update
+sudo apt-get update
 
 echo 'Install postgresql'
-sudo apt-get -qq -y install postgresql-14 > /dev/null
+sudo apt-get -y install postgresql-14
 
 echo 'Install tools'
-sudo apt-get -qq -y install mc > /dev/null
+sudo apt-get -y install mc
 
 echo 'Update postgresql configurations'
 sudo cp /vagrant/postgresql.conf /etc/postgresql/14/main/postgresql.conf
