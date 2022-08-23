1cce3a0b9ffe71f786c7b7e07d9ce21c240969ab
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2022-08-23 22:22:11 +0300

    infinispan

--- library/lib-infinispan/src/main/java/ua/in/sz/hibernate/cache/Application.java
+++ library/lib-infinispan/src/main/java/ua/in/sz/hibernate/cache/Application.java
@@ -5,14 +5,23 @@ import org.infinispan.Cache;
 import org.infinispan.manager.DefaultCacheManager;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class Application {
 	public static void main(String[] args) throws IOException {
 		DefaultCacheManager cacheManager = new DefaultCacheManager(Application.class.getResourceAsStream("/weatherapp-infinispan.xml"));
-		Cache<Object, Object> cache = cacheManager.getCache("weather");
+		Cache<String, String> cache = cacheManager.getCache("weather");
 		cache.put("1", "One");
 		cache.put("2", "Two");
+
+		for (int i = 0; i < 1_000_000; i++) {
+			List<Map.Entry<String, String>> entities = cache.entrySet().stream().toList();
+			if (entities.isEmpty()) {
+				throw new IllegalStateException();
+			}
+		}
 		log.info("1={}", cache.get("1"));
 	}
 }
