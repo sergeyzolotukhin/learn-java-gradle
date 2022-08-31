eb80870f37698832583563d41e61c58c2c6cefb6
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2022-09-01 00:42:54 +0300

    infinispan

--- library/lib-cxf-rs/src/main/java/ua/in/sz/cxf/server/SimpleExceptionMapper.java
+++ library/lib-cxf-rs/src/main/java/ua/in/sz/cxf/server/SimpleExceptionMapper.java
@@ -8,7 +8,7 @@ import javax.ws.rs.ext.Provider;
 public class SimpleExceptionMapper implements ExceptionMapper<IllegalStateException> {
     public Response toResponse(IllegalStateException ex) {
         return Response.status(Response.Status.BAD_REQUEST).
-                entity("HOW TO GET A MESSAGE FROM AN EXCEPTION IN HERE???")
+                entity("HOW TO GET A MESSAGE FROM AN EXCEPTION IN HERE??? " + ex.getMessage())
                 .build();
     }
 }
