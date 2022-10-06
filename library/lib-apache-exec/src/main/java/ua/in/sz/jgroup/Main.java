8c7299d60edb3ca8e3420227f7b55132463983d5
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2022-10-06 21:30:52 +0300

    Command line run

---
+++ library/lib-apache-exec/src/main/java/ua/in/sz/jgroup/Main.java
@@ -0,0 +1,29 @@
+package ua.in.sz.jgroup;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+
+import java.util.Arrays;
+
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        CommandLine mvn = new CommandLine("mvn");
+        mvn.addArgument("--version");
+
+        CommandLine cmd = new CommandLine("cmd");
+        cmd.addArgument("/c");
+        cmd.addArguments(mvn.toStrings(), true);
+
+        log.info("CMD:{}", Arrays.asList(cmd.toStrings()));
+
+        DefaultExecutor executor = new DefaultExecutor();
+        int exitCode = executor.execute(cmd);
+
+        log.info("Command executed: {} with exit code: {}", cmd, exitCode);
+    }
+
+
+}
