4aad35b457e5c4e791afe59fa5b45c4d08e38170
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2022-11-17 19:57:50 +0200

    spring message sources

--- spring/spring-message-source/src/main/java/ua/in/sz/spring/Application.java
+++ spring/spring-message-source/src/main/java/ua/in/sz/spring/Application.java
@@ -3,10 +3,14 @@ package ua.in.sz.spring;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Locale;
 
 @Slf4j
@@ -14,9 +18,10 @@ import java.util.Locale;
 @ImportResource("classpath:app-config.xml")
 public class Application {
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
-		ReloadableResourceBundleMessageSource messageSource = ctx.getBean(ReloadableResourceBundleMessageSource.class);
-		String message = messageSource.getMessage("message.1", new Object[0], Locale.getDefault());
-		log.info("Message: [{}]", message);
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		MessageSource messageSource = ctx.getBean(ReloadableResourceBundleMessageSource.class);
+		Object[] arguments = new Object[]{BigDecimal.valueOf(1.2224), LocalDate.now()};
+		String message = messageSource.getMessage("message.1", arguments, Locale.getDefault());
+		log.info("{}", message);
 	}
 }
