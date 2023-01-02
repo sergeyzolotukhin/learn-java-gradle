162e87ac6fd3dd4c33a787aaf2bbfe1e30d968aa
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-01-03 00:07:03 +0200

    Home power

--- house-power/src/main/java/ua/in/sz/home/power/Main.java
+++ house-power/src/main/java/ua/in/sz/home/power/Main.java
@@ -18,6 +18,7 @@ import java.util.TimeZone;
 // https://javadevblog.com/polnoe-rukovodstvo-po-java-8-date-time-api-primery-localdate-instant-localdatetime-parse-i-format.html
 // https://jcp.org/aboutJava/communityprocess/pfd/jsr310/JSR-310-guide.html
 // https://www.baeldung.com/java-zoneddatetime-offsetdatetime
+// https://lurninghut.com/localdatetime-vs-zoneddatetime/
 @Slf4j
 public class Main {
     private static final DateTimeFormatter HH_MM = DateTimeFormatter.ofPattern("HH:mm");
