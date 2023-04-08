7fb21de9a597d684ce9f59013362f51e318bf144
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-04-08 16:37:32 +0300

    Home power

---
+++ library/lib-junit5-extension/src/main/java/ua/in/sz/junit5/Main.java
@@ -0,0 +1,10 @@
+package ua.in.sz.junit5;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("Hello world!");
+    }
+}
