7d2d1bb2904b11bb2225134df0dde218ffd5a20c
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-04-09 23:52:03 +0300

    Home power

--- library/lib-h2/src/main/java/ua/in/sz/h2/Main.java
+++ library/lib-h2/src/main/java/ua/in/sz/h2/Main.java
@@ -1,6 +1,7 @@
 package ua.in.sz.h2;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.StopWatch;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,17 +15,37 @@ public class Main {
         var url = "jdbc:h2:mem:";
 
         try (var con = DriverManager.getConnection(url)) {
-             var stm = con.createStatement();
+            var stm = con.createStatement();
 
-             stm.execute("CREATE TABLE TEST(ID INT PRIMARY KEY, NAME VARCHAR(255)) " +
-                     "AS SELECT * FROM CSVREAD('D:/projects-java/_learn_framework/patterns/library/lib-h2/src/main/resources/test.csv')");
+            StopWatch sw = StopWatch.createStarted();
 
-            var rs = stm.executeQuery("SELECT * FROM TEST");
+            stm.execute("CREATE TABLE TEST(ID INT PRIMARY KEY, NAME VARCHAR(255)) "
+//                            +
+//                    "AS SELECT * FROM CSVREAD('D:/projects-java/_learn_framework/patterns/library/lib-h2/src/main/resources/test.csv')"
+            );
+
+            String sql = "insert into TEST (ID, NAME) values (?, ?)";
+            con.setAutoCommit(false);
+            try (var ps = con.prepareStatement(sql)) {
+                for (int i = 3; i < 100_000; i++) {
+                    ps.setInt(1, i);
+                    ps.setString(2, String.format("Name_%d", i));
+                    ps.addBatch();
+                }
+                ps.executeBatch();
+                ps.clearBatch();
+            }
+            con.commit();
+            // Database creation took 00:00:00.27
+
+            log.info("Database creation took {}", sw);
+
+            var rs = stm.executeQuery("SELECT count(*) FROM TEST");
             while (rs.next()) {
-                log.info("{} -> {}", rs.getInt(1), rs.getString(2));
+                log.info("CountL {}", rs.getLong(1));
             }
 
-            stm.execute("CALL CSVWRITE('library/lib-h2/build/test_wr.csv', 'SELECT * FROM TEST')");
+//            stm.execute("CALL CSVWRITE('library/lib-h2/build/test_wr.csv', 'SELECT * FROM TEST')");
         } catch (SQLException e) {
             log.error("Can not open connection", e);
         }
