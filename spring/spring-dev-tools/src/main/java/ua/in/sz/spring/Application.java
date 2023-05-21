23d69da98552f18520e94bd1071d9e87c73a2050
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-05-21 13:17:18 +0300

    patterns-12 How to track time and context in IDEA

--- spring/spring-dev-tools/src/main/java/ua/in/sz/spring/Application.java
+++ spring/spring-dev-tools/src/main/java/ua/in/sz/spring/Application.java
@@ -10,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
+		// Just a comment
 		SpringApplication.run(Application.class, args);
 	}
 }
