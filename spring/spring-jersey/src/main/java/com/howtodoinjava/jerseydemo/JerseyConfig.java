7a11da5055742b5b31154531adb9226bd5984742
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-06-03 20:50:54 +0300

    My new task

--- spring/spring-jersey/src/main/java/com/howtodoinjava/jerseydemo/JerseyConfig.java
+++ spring/spring-jersey/src/main/java/com/howtodoinjava/jerseydemo/JerseyConfig.java
@@ -3,10 +3,7 @@ package com.howtodoinjava.jerseydemo;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
-//import jakarta.ws.rs.ApplicationPath;
-
 @Component
-//@ApplicationPath("/rest")
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         register(UserResource.class);
