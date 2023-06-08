f01e21d01e1f00aa1864f72f01af193dcb2e1f41
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-06-08 22:52:39 +0300

    My new task

--- library/lib-quartz/src/main/java/ua/in/sz/quartz/calendar/only/AppCalendarOnly.java
+++ library/lib-quartz/src/main/java/ua/in/sz/quartz/calendar/only/AppCalendarOnly.java
@@ -11,7 +11,7 @@ import java.time.ZonedDateTime;
 public class AppCalendarOnly {
     public static void main(String[] args) throws Exception {
         CronCalendar calendar = new CronCalendar("0 0 0 ? * SAT-SUN");
-        WorkdayCalendar workdayCalendar = new WorkdayCalendar("2-10", calendar);
+        WorkdayCalendar workdayCalendar = new WorkdayCalendar("2-10,18-22", calendar);
         CronCalendar wn = new CronCalendar(workdayCalendar,"0 0 0 ? * MON-TUE,THU-SUN");
 
         log.info("expression: [{}]", workdayCalendar.getExpression());
