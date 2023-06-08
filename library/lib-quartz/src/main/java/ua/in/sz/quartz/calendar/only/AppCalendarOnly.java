4d40d2cf69ce82bd31a29815e8f5ad24cf04c224
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-06-08 21:54:03 +0300

    My new task

--- library/lib-quartz/src/main/java/ua/in/sz/quartz/calendar/only/AppCalendarOnly.java
+++ library/lib-quartz/src/main/java/ua/in/sz/quartz/calendar/only/AppCalendarOnly.java
@@ -10,7 +10,10 @@ import java.time.ZonedDateTime;
 @Slf4j
 public class AppCalendarOnly {
     public static void main(String[] args) throws Exception {
-        CronCalendar calendar = new CronCalendar("0 0 0 ? * MON-FRI");
+        CronCalendar calendar = new CronCalendar("0 0 0 ? * SAT-SUN");
+        WorkdayCalendar workdayCalendar = new WorkdayCalendar("2-4,6-8");
+
+        log.info("expression: [{}]", workdayCalendar.getExpression());
 
         LocalDateTime date = LocalDateTime.of(2023, 6, 1, 0, 0, 0);
         for (int i = 0; i < date.toLocalDate().lengthOfMonth(); i++) {
@@ -18,10 +21,9 @@ public class AppCalendarOnly {
             ZonedDateTime zdt = ZonedDateTime.of(localDate, ZoneId.systemDefault());
             long tm = zdt.toInstant().toEpochMilli();
 //            long nextIncludedTime = calendar.getNextIncludedTime(tm);
-            boolean timeIncluded = calendar.isTimeIncluded(tm);
+            boolean timeIncluded = workdayCalendar.isTimeIncluded(tm);
 
             log.info("{} => {}", localDate.toLocalDate(), timeIncluded);
-
         }
 
 
---
+++ library/lib-quartz/src/main/java/ua/in/sz/quartz/calendar/only/WorkdayCalendar.java
@@ -0,0 +1,58 @@
+package ua.in.sz.quartz.calendar.only;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.Calendar;
+import org.quartz.impl.calendar.BaseCalendar;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+@Slf4j
+public class WorkdayCalendar extends BaseCalendar implements Calendar, Serializable {
+    private final int holiday;
+    @Getter
+    private final String expression;
+
+    public WorkdayCalendar(String expression) {
+        this(expression, null);
+    }
+
+    public WorkdayCalendar(String expression, Calendar baseCalendar) {
+        super(baseCalendar);
+        this.expression = expression;
+        holiday = buildExpression(expression);
+    }
+
+    public boolean isTimeIncluded(long timeStamp) {
+        LocalDate date = Instant.ofEpochMilli(timeStamp).atZone(ZoneId.systemDefault()).toLocalDate();
+        int dayOfMonth = date.getDayOfMonth() - 1;
+        int mask = 1 << dayOfMonth;
+        int result = holiday & mask;
+//        log.info("mask: [{}] result [{}]", Integer.toBinaryString(mask), Integer.toBinaryString(result));
+        return result > 0;
+    }
+
+    private int buildExpression(String expression) {
+        int holiday = 0;
+
+        String[] rages = expression.split(",");
+        for (String rage : rages) {
+            String[] bounds = rage.split("-");
+            int from = Integer.parseInt(bounds[0]);
+            int to = Integer.parseInt(bounds[1]);
+
+            for (int i = from; i <= to; i++) {
+                holiday |= 1 << i;
+//                log.info("Range {} -> {}", rage, i);
+            }
+        }
+
+//        String str = String.format("%32s", Integer.toBinaryString(holiday)).replaceAll(" ", "0");
+//        log.info("{}", str);
+
+        return holiday;
+    }
+}
