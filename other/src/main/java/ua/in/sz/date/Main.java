aba93447831f17380cb18f1f0466a6ff4c26e21e
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-06-13 20:53:37 +0300

    My new task

---
+++ other/src/main/java/ua/in/sz/date/Main.java
@@ -0,0 +1,19 @@
+package ua.in.sz.date;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        Date d = new Date();
+
+        log.info("{}", d);
+
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.DATE, 1);
+        log.info("{}", c.getTime());
+    }
+}
