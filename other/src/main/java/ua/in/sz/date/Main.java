96a50090e19883b56b8d96d5a9a39b4c5c2e4857
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-06-13 20:55:03 +0300

    My new task

--- other/src/main/java/ua/in/sz/date/Main.java
+++ other/src/main/java/ua/in/sz/date/Main.java
@@ -9,7 +9,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date d = new Date();
-
         log.info("{}", d);
 
         Calendar c = Calendar.getInstance();
