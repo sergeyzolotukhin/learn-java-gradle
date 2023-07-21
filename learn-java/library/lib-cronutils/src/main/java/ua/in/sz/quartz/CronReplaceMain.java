6e5716c59f8b5330a867f75e5bd07ed0a7a6d1bd
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-07-22 00:00:46 +0300

    Remove house project

---
+++ learn-java/library/lib-cronutils/src/main/java/ua/in/sz/quartz/CronReplaceMain.java
@@ -0,0 +1,58 @@
+package ua.in.sz.quartz;
+
+import com.cronutils.model.Cron;
+import com.cronutils.model.CronType;
+import com.cronutils.model.SingleCron;
+import com.cronutils.model.definition.CronDefinition;
+import com.cronutils.model.definition.CronDefinitionBuilder;
+import com.cronutils.model.field.CronField;
+import com.cronutils.model.field.CronFieldName;
+import com.cronutils.model.field.expression.FieldExpression;
+import com.cronutils.model.field.expression.On;
+import com.cronutils.model.field.expression.visitor.FieldExpressionVisitorAdaptor;
+import com.cronutils.model.field.value.IntegerFieldValue;
+import com.cronutils.model.field.value.SpecialChar;
+import com.cronutils.model.field.value.SpecialCharFieldValue;
+import com.cronutils.parser.CronParser;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class CronReplaceMain {
+    @SneakyThrows
+    public static void main(String[] args) {
+        CronDefinition cronDefinition = CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ);
+        CronParser parser = new CronParser(cronDefinition);
+
+        Cron cron = parser.parse("0 0 12 ? * 1#4");
+        SingleCron normalizedCron = normalize(cron);
+
+        log.info("{} -> {}", cron.asString(), normalizedCron.asString());
+    }
+
+    private static SingleCron normalize(Cron cron) {
+        List<CronField> fields = cron.retrieveFieldsAsMap().values().stream()
+                .map(CronReplaceMain::normalizeDayOfWeekField)
+                .collect(Collectors.toList());
+
+        return new SingleCron(cron.getCronDefinition(), fields);
+    }
+
+    private static CronField normalizeDayOfWeekField(CronField field) {
+        if (!CronFieldName.DAY_OF_WEEK.equals(field.getField())) {
+            return field;
+        }
+
+        FieldExpression expression = field.getExpression().accept(new NthNormalizeVisitor());
+        return new CronField(field.getField(), expression, field.getConstraints());
+    }
+
+    private static class NthNormalizeVisitor extends FieldExpressionVisitorAdaptor {
+        public FieldExpression visit(On on) {
+            return new On(on.getTime(), new SpecialCharFieldValue(SpecialChar.NONE), new IntegerFieldValue(-1));
+        }
+    }
+}
