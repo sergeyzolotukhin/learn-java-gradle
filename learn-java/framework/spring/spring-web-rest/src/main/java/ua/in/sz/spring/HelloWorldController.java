a2b2ac6af30f9e535eab9ed67e427b403f3bf613
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-08-05 20:26:59 +0300

    Remove house project

--- learn-java/framework/spring/spring-web-rest/src/main/java/ua/in/sz/spring/HelloWorldController.java
+++ learn-java/framework/spring/spring-web-rest/src/main/java/ua/in/sz/spring/HelloWorldController.java
@@ -1,15 +1,18 @@
 package ua.in.sz.spring;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class HelloWorldController {
     @GetMapping("/hello")
     public List<String> findByPublished() {
+        log.info("A get request has received");
         return List.of("Hello", "Serhij", "Zolotukhin");
     }
 }
