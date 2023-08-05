2e46cde58783fef7492bb69c9e63375a51c83dcd
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-08-05 16:21:08 +0300

    Remove house project

--- learn-java/framework/spring/spring-web-rest/src/main/java/ua/in/sz/spring/HelloWorldController.java
+++ learn-java/framework/spring/spring-web-rest/src/main/java/ua/in/sz/spring/HelloWorldController.java
@@ -1,8 +1,6 @@
 package ua.in.sz.spring;
 
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +9,7 @@ import java.util.List;
 @RestController
 public class HelloWorldController {
     @GetMapping("/hello")
-    public ResponseEntity<List<String>> findByPublished() {
-        return new ResponseEntity<>(List.of("Hello", "Serhij", "Zolotukhin"), HttpStatus.OK);
+    public List<String> findByPublished() {
+        return List.of("Hello", "Serhij", "Zolotukhin");
     }
 }
