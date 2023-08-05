9aa3879551f12e7efaea60a9f9d115decf2488a8
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-08-05 17:43:21 +0300

    Remove house project

--- learn-java/library/lib-http-client/README.md
+++ learn-java/library/lib-http-client/README.md
@@ -1,3 +1,8 @@
 ### It is based on the following tutorial
 
 https://hc.apache.org/httpcomponents-client-4.5.x/index.html
+
+### How to manage connection pools in the Http Client
+
+2.3.3. Pooling connection manager
+https://hc.apache.org/httpcomponents-client-4.5.x/current/tutorial/html/connmgmt.html
--- learn-java/library/lib-http-client/src/main/java/ua/in/sz/spring/Application.java
+++ learn-java/library/lib-http-client/src/main/java/ua/in/sz/spring/Application.java
@@ -6,6 +6,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
@@ -13,13 +14,21 @@ import java.io.IOException;
 @Slf4j
 public class Application {
     public static void main(String[] args) throws IOException {
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            HttpGet httpGet = new HttpGet("http://localhost:8080/hello");
-            try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
-                log.info("Status line: [{}]", response.getStatusLine());
+        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+        cm.setMaxTotal(20);
+        cm.setDefaultMaxPerRoute(20);
 
-                HttpEntity entity = response.getEntity();
-                log.info("Entity: [{}]", EntityUtils.toString(entity));
+        try (CloseableHttpClient httpclient = HttpClients.custom().setConnectionManager(cm).build()) {
+            HttpGet httpGet = new HttpGet("http://localhost:8080/hello");
+            for (int i = 0; i < 200; i++) {
+                // We can remove the try with a resource to emulate connection leak
+                try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
+                    log.info("Status line: [{}]", response.getStatusLine().getStatusCode());
+
+                    // We can comment those line to emulate connection leak
+                    HttpEntity entity = response.getEntity();
+                    log.info("Entity: {}", EntityUtils.toString(entity));
+                }
             }
         }
     }
