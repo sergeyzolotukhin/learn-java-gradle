e28d8d58e2e7691bf216606eda547575521c0bdd
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-09-19 22:10:42 +0300

    Jenkins

--- learn-java/library/jenkins-rest/src/main/java/ua/in/sz/h2/Main.java
+++ learn-java/library/jenkins-rest/src/main/java/ua/in/sz/h2/Main.java
@@ -1,8 +1,11 @@
 package ua.in.sz.h2;
 
 import com.cdancy.jenkins.rest.JenkinsClient;
+import com.cdancy.jenkins.rest.domain.common.IntegerResponse;
 import com.cdancy.jenkins.rest.domain.system.SystemInfo;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
 
 @Slf4j
 public class Main {
@@ -16,5 +19,15 @@ public class Main {
         SystemInfo systemInfo = client.api().systemApi().systemInfo();
 
         log.info("Jenkins version: [{}]", systemInfo.jenkinsVersion());
+
+        Map<String, List<String>> parameters = new HashMap<>();
+        parameters.put("ARG_1", Collections.singletonList("SZ 1" + new Date()));
+        parameters.put("ARG_2", Collections.singletonList("SZ 2" + new Date()));
+        parameters.put("ARG_3", Collections.singletonList("SZ 3" + new Date()));
+
+
+        IntegerResponse build = client.api().jobsApi().buildWithParameters(null, "job_4", parameters);
+
+        log.info("Jenkins build: [{}]", build.value());
     }
 }
