7bf7e809c0fcbc1da40c90264b4c4f5dbfbb6f67
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-09-26 23:24:38 +0300

    Jenkins

--- learn-java/core/task-executor/src/main/java/ua/in/sz/h2/p2/ChildClass.java
+++ learn-java/core/task-executor/src/main/java/ua/in/sz/h2/p2/ChildClass.java
@@ -1,5 +1,8 @@
 package ua.in.sz.h2.p2;
 
 public class ChildClass extends BaseClass {
-
+//    @Override
+//    public String getName() {
+//        return super.getName() + " add text" ;
+//    }
 }
