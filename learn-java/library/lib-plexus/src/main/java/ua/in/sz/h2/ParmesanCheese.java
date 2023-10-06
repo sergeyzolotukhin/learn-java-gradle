f626eff54d86a4874079ad289121e3e3b87a6570
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-10-07 00:03:38 +0300

    Jenkins

--- learn-java/library/lib-plexus/src/main/java/ua/in/sz/h2/ParmesanCheese.java
+++ learn-java/library/lib-plexus/src/main/java/ua/in/sz/h2/ParmesanCheese.java
@@ -4,8 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ParmesanCheese implements Cheese {
+    private Second second;
+    private Second other;
+
     @Override
     public void print() {
         log.info("Hello Serhij 1111");
+
+        second.print();
+
+        other.print();
     }
 }
