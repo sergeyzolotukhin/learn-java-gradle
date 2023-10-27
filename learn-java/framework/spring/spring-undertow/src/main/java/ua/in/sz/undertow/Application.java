1c3d592d259cbc0ee5592566b5d5b553b5d5b595
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-10-28 00:25:02 +0300

    undertow jboss

--- learn-java/framework/spring/spring-undertow/src/main/java/ua/in/sz/undertow/Application.java
+++ learn-java/framework/spring/spring-undertow/src/main/java/ua/in/sz/undertow/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        log.info("Hello world!");
         SpringApplication.run(Application.class, args);
+        log.info("Hello world!");
     }
 }
