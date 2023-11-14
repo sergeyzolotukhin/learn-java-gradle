aa643428a8123b63d1048bddd646905b6f543614
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-11-14 19:09:26 +0200

    undertow jboss

--- learn-java/library/lib-camel/src/main/java/ua/in/sz/camel/Application.java
+++ learn-java/library/lib-camel/src/main/java/ua/in/sz/camel/Application.java
@@ -4,24 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.support.jndi.JndiContext;
 import ua.in.sz.camel.enpoints.FtpEndpointImpl;
 import ua.in.sz.camel.enpoints.IllegalStateExceptionFtpEndpointImpl;
 import ua.in.sz.camel.enpoints.RuntimeExceptionFtpEndpointImpl;
 
-;
-
 @Slf4j
 public class Application {
 	public static void main(String[] args) throws Exception {
-		JndiContext jndiContext = new JndiContext();
+		CamelContext camelContext = new DefaultCamelContext();
 
-		jndiContext.bind("ftp-1", new IllegalStateExceptionFtpEndpointImpl("ftp-1"));
-		jndiContext.bind("ftp-2", new FtpEndpointImpl("ftp-2"));
-		jndiContext.bind("ftp-3", new RuntimeExceptionFtpEndpointImpl("ftp-3"));
-		jndiContext.bind("ftp-4", new RuntimeExceptionFtpEndpointImpl("ftp-4"));
+		camelContext.getRegistry().bind("ftp-1", new IllegalStateExceptionFtpEndpointImpl("ftp-1"));
+		camelContext.getRegistry().bind("ftp-2", new FtpEndpointImpl("ftp-2"));
+		camelContext.getRegistry().bind("ftp-3", new RuntimeExceptionFtpEndpointImpl("ftp-3"));
+		camelContext.getRegistry().bind("ftp-4", new RuntimeExceptionFtpEndpointImpl("ftp-4"));
 
-		CamelContext camelContext = new DefaultCamelContext(jndiContext);
 		try {
 			camelContext.addRoutes(new FtpRouteBuilder());
 			ProducerTemplate template = camelContext.createProducerTemplate();
