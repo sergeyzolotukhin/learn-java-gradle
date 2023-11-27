3f5c4acb4366734bd89d2bb93458e8ba03f5faf2
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-11-27 22:20:37 +0200

    undertow jboss

--- learn-java/framework/spring-boot/spring-jersey/src/test/java/ua/in/sz/jersey/ApplicationTests.java
+++ learn-java/framework/spring-boot/spring-jersey/src/test/java/ua/in/sz/jersey/ApplicationTests.java
@@ -5,6 +5,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ import java.nio.charset.StandardCharsets;
 
 public class ApplicationTests {
     @Test
+    @Disabled
     public void getUser() throws IOException {
         HttpUriRequest request = new HttpGet( "http://localhost:8080/users/1");
 
