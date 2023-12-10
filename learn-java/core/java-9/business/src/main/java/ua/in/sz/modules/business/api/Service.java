4f11ae66f18b2f7970476fc92f26aeddaa16fcb8
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-12-10 23:08:22 +0200

    java

--- learn-java/core/java-9/business/src/main/java/ua/in/sz/modules/business/api/Service.java
+++ learn-java/core/java-9/business/src/main/java/ua/in/sz/modules/business/api/Service.java
@@ -3,16 +3,12 @@ package ua.in.sz.modules.business.api;
 import lombok.extern.slf4j.Slf4j;
 import ua.in.sz.modules.persistence.api.Repository;
 import ua.in.sz.modules.persistence.api.dto.ValueDto;
-//import ua.in.sz.modules.persistence.impl.RepositoryImpl;
 
 @Slf4j
 public class Service {
 
     public String print() {
         Repository repository = new Repository();
-
-//        RepositoryImpl implementation = new RepositoryImpl();
-//        implementation.save();
 
         log.info("Service");
         ValueDto save = repository.save();
