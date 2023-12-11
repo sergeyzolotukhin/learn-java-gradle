8a96f2ca257204d5628c659d9e574cedf5586690
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-12-12 00:27:06 +0200

    java

--- learn-java/library/lib-tomcat/src/main/java/ua/in/sz/undertow/Main.java
+++ learn-java/library/lib-tomcat/src/main/java/ua/in/sz/undertow/Main.java
@@ -1,36 +1,47 @@
 package ua.in.sz.undertow;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.WebResourceRoot;
-import org.apache.catalina.core.StandardContext;
-import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.DirResourceSet;
-import org.apache.catalina.webresources.StandardRoot;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) throws Exception {
-
-        String webappDirLocation = "learn-java/library/lib-tomcat/src/main/webapp/";
         Tomcat tomcat = new Tomcat();
-
+        tomcat.setBaseDir("temp");
         tomcat.setPort(8080);
 
-        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
-        log.info("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+        String contextPath = "/";
+        String docBase = new File(".").getAbsolutePath();
 
-        File additionWebInfClasses = new File("learn-java/library/lib-tomcat/build/classes");
+        Context context = tomcat.addContext(contextPath, docBase);
 
-        WebResourceRoot resources = new StandardRoot(ctx);
-        resources.addPreResources(new DirResourceSet(
-                resources,
-                "/WEB-INF/classes",
-                additionWebInfClasses.getAbsolutePath(),
-                "/"));
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+                    throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
 
-        ctx.setResources(resources);
+                writer.println("<html><title>Welcome</title><body>");
+                writer.println("<h1>Have a Great Day!</h1>");
+                writer.println("</body></html>");
+            }
+        };
+
+        String servletName = "Servlet1";
+        String urlPattern = "/go";
+
+        tomcat.addServlet(contextPath, servletName, servlet);
+        context.addServletMappingDecoded(urlPattern, servletName);
 
         tomcat.start();
         tomcat.getServer().await();
