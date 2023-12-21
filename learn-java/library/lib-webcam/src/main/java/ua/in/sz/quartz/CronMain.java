6c8240e21e85a2ccdcde4ce745bb439a959efb97
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-12-21 20:55:46 +0200

    java

--- learn-java/library/lib-webcam/src/main/java/ua/in/sz/quartz/CronMain.java
+++ learn-java/library/lib-webcam/src/main/java/ua/in/sz/quartz/CronMain.java
@@ -1,24 +1,41 @@
 package ua.in.sz.quartz;
 
-import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamResolution;
-import com.github.sarxos.webcam.util.ImageUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.bytedeco.opencv.opencv_core.IplImage;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import javax.swing.*;
+import java.awt.event.WindowEvent;
+
+import static org.bytedeco.opencv.helper.opencv_imgcodecs.cvSaveImage;
 
 @Slf4j
 public class CronMain {
-    public static void main(String[] args) throws IOException {
-        Webcam webcam = Webcam.getDefault();
-        webcam.setViewSize(WebcamResolution.VGA.getSize());
-        webcam.open();
+    public static void main(String[] args) throws Exception {
+        CanvasFrame canvas = new CanvasFrame("Web Cam");
+        canvas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        BufferedImage image = webcam.getImage();
+        FrameGrabber grabber = new OpenCVFrameGrabber(0);
 
-        ImageIO.write(image, ImageUtils.FORMAT_JPG, new File("selfie.jpg"));
+        grabber.setImageHeight(1080);
+        grabber.setImageWidth(1920);
+
+        OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
+
+        grabber.start();
+        Frame frame = grabber.grab();
+
+        IplImage img = converter.convert(frame);
+        cvSaveImage("selfie-1.jpg", img);
+
+        canvas.showImage(frame);
+
+        Thread.sleep(2000);
+
+        canvas.dispatchEvent(new WindowEvent(canvas, WindowEvent.WINDOW_CLOSING));
     }
 }
