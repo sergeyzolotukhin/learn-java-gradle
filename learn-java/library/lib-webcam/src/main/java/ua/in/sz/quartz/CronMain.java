ceec297dc038278ddc72c0c08b6f5aa6085385a6
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-12-21 20:33:24 +0200

    java

---
+++ learn-java/library/lib-webcam/src/main/java/ua/in/sz/quartz/CronMain.java
@@ -0,0 +1,26 @@
+package ua.in.sz.quartz;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.swing.*;
+
+@Slf4j
+public class CronMain {
+    public static void main(String[] args) {
+        Webcam webcam = Webcam.getDefault();
+        webcam.setViewSize(WebcamResolution.VGA.getSize());
+
+        WebcamPanel panel = new WebcamPanel(webcam);
+        panel.setImageSizeDisplayed(true);
+
+        JFrame window = new JFrame("Webcam");
+        window.add(panel);
+        window.setResizable(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        window.setVisible(true);
+    }
+}
