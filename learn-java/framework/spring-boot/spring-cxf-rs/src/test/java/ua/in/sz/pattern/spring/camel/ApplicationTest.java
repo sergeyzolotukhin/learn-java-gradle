3bba3e922c047a7e3e449e6a66371803c462c87b
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-02-17 14:01:29 +0200

    java

--- learn-java/framework/spring-boot/spring-cxf-rs/src/test/java/ua/in/sz/pattern/spring/camel/ApplicationTest.java
+++ learn-java/framework/spring-boot/spring-cxf-rs/src/test/java/ua/in/sz/pattern/spring/camel/ApplicationTest.java
@@ -1,22 +1,21 @@
 package ua.in.sz.pattern.spring.camel;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
+import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ApplicationTest {
 
-
-    @BeforeEach
-    void setUp() {
-
-    }
-
     @Test
     void endpoint() {
-
+        WebRsService client = JAXRSClientFactory.create("http://localhost:8080/ws/api/", WebRsService.class);
+        String result = client.sayHi("Serhij Zolotukhin");
+        log.info("RESULT: {}", result);
+        assertEquals("Hello Serhij Zolotukhin", result);
     }
 }
