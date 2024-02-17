f12c90669b6ab3c863cd8e265fad9fc7739addee
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-02-17 17:28:24 +0200

    java

--- learn-java/framework/spring-boot/spring-cxf-rs/src/test/java/ua/in/sz/pattern/spring/camel/ApplicationTest.java
+++ learn-java/framework/spring-boot/spring-cxf-rs/src/test/java/ua/in/sz/pattern/spring/camel/ApplicationTest.java
@@ -1,5 +1,7 @@
 package ua.in.sz.pattern.spring.camel;
 
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.MediaType;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.cxf.jaxrs.client.Client;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
@@ -19,10 +21,21 @@ class ApplicationTest {
     void endpoint() {
         WebRsService proxy = JAXRSClientFactory.create("http://localhost:8080/ws/api/", WebRsService.class);
         Client client = WebClient.client(proxy);
-        client.header("Authorization", "Basic " +  Base64.getEncoder().encodeToString(("admin:admin").getBytes()));
+        client.authorization("Basic " +  Base64.getEncoder().encodeToString(("admin:admin").getBytes()));
 
         String result = proxy.sayHi("Serhij Zolotukhin");
         log.info("RESULT: {}", result);
         assertEquals("Hello Serhij Zolotukhin", result);
+    }
+
+    @Test
+    void endpoint_2() {
+        String result = ClientBuilder.newClient()
+                .target("http://localhost:8080/ws/api/hello/message")
+                .request(MediaType.TEXT_PLAIN)
+                .header("Authorization", "Basic " +  Base64.getEncoder().encodeToString(("admin:admin").getBytes()))
+                .get(String.class);
+
+        log.info("RESULT: {}", result);
     }
 }
