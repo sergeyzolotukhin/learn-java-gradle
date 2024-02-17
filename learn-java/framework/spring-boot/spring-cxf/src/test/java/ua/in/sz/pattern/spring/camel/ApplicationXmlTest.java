51198d66306bea427dd97ce00b938f3213539a3e
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-02-17 21:32:53 +0200

    java

---
+++ learn-java/framework/spring-boot/spring-cxf/src/test/java/ua/in/sz/pattern/spring/camel/ApplicationXmlTest.java
@@ -0,0 +1,31 @@
+package ua.in.sz.pattern.spring.camel;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.xml.namespace.QName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration({"classpath:/test-application-context.xml"})
+class ApplicationXmlTest {
+
+    @Autowired
+    private WebService testClient;
+
+    @Test
+    void endpoint() {
+        String result = testClient.sayHi("General Kenobi");
+        log.info("soap response: [{}]", result);
+        assertEquals("Hello General Kenobi", result);
+    }
+}
