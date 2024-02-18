fac6f3004058fd6df9a7b23060d15f201eeb21ed
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-02-18 14:37:06 +0200

    java

--- learn-java/framework/spring-boot/spring-web-rest/src/main/java/ua/in/sz/spring/SecurityConfig.java
+++ learn-java/framework/spring-boot/spring-web-rest/src/main/java/ua/in/sz/spring/SecurityConfig.java
@@ -3,6 +3,7 @@ package ua.in.sz.spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -29,7 +31,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> {
                     authorize.anyRequest().authenticated();
                 })
-                .httpBasic(withDefaults());
+                .httpBasic(withDefaults())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
 
         http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.NEVER));
 
