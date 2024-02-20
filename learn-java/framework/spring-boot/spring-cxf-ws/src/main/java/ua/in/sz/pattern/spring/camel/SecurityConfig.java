3808a62c28d73afd6cd6d2e460b2daae6dda5464
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-02-20 20:00:12 +0200

    java

--- learn-java/framework/spring-boot/spring-cxf-ws/src/main/java/ua/in/sz/pattern/spring/camel/SecurityConfig.java
+++ learn-java/framework/spring-boot/spring-cxf-ws/src/main/java/ua/in/sz/pattern/spring/camel/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,7 +17,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> {
                     authorize.anyRequest().authenticated();
                 })
@@ -36,6 +38,10 @@ public class SecurityConfig {
                 .username("admin").password(passwordEncoder().encode("admin")).roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(admin);
+        UserDetails user = User.builder()
+                .username("user").password(passwordEncoder().encode("user")).roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
--- learn-java/framework/spring-boot/spring-cxf-ws/src/test/java/ua/in/sz/pattern/spring/camel/ApplicationTest.java
+++ learn-java/framework/spring-boot/spring-cxf-ws/src/test/java/ua/in/sz/pattern/spring/camel/ApplicationTest.java
@@ -4,8 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.cxf.configuration.security.AuthorizationPolicy;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.transport.http.HTTPConduit;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,6 +25,8 @@ class ApplicationTest {
 
     @BeforeEach
     void setUp() {
+//        log.info("Started {}", this);
+
         JaxWsProxyFactoryBean jaxWsProxyFactory = new JaxWsProxyFactoryBean();
         jaxWsProxyFactory.setServiceClass(WebService.class);
         jaxWsProxyFactory.setServiceName(new QName(
@@ -29,6 +34,8 @@ class ApplicationTest {
                 "WebServiceImplService"
         ));
         jaxWsProxyFactory.setAddress("http://localhost:8080/ws/Hello");
+//        jaxWsProxyFactory.getInInterceptors().add(new LoggingInInterceptor());
+//        jaxWsProxyFactory.getOutInterceptors().add(new LoggingOutInterceptor());
         testClient = jaxWsProxyFactory.create(WebService.class);
 
         AuthorizationPolicy authorizationPolicy = new AuthorizationPolicy();
@@ -41,10 +48,30 @@ class ApplicationTest {
         conduit.setAuthorization(authorizationPolicy);
     }
 
+    @AfterEach
+    void tearDown() {
+//        log.info("Ended {}", this);
+    }
+
     @Test
     void endpoint() {
         String result = testClient.sayHi("General Kenobi");
         log.info("soap response: [{}]", result);
-        assertEquals("Hello General Kenobi", result);
+        assertEquals("Hello General Kenobi admin", result);
+
+        AuthorizationPolicy authorizationPolicy = new AuthorizationPolicy();
+        authorizationPolicy.setUserName("user");
+        authorizationPolicy.setPassword("user");
+        authorizationPolicy.setAuthorizationType("Basic");
+
+        Client client = ClientProxy.getClient(testClient);
+        HTTPConduit conduit = (HTTPConduit)client.getConduit();
+        conduit.setAuthorization(authorizationPolicy);
+
+        String result2 = testClient.sayHi("General Kenobi");
+        log.info("soap response: [{}]", result2);
+        assertEquals("Hello General Kenobi user", result2);
+
+        log.info("Ended");
     }
 }
