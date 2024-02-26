138fdee2e80cde554a46723218c88faf2bbbeca6
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-02-26 20:53:27 +0200

    java

---
+++ learn-java/framework/spring-boot/spring-property/src/main/java/ua/in/sz/pattern/spring/property/AppListener.java
@@ -0,0 +1,35 @@
+package ua.in.sz.pattern.spring.property;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ConfigurableBootstrapContext;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertySource;
+
+import java.util.Map;
+
+@Slf4j
+public class AppListener implements SpringApplicationRunListener {
+    @Override
+    public void environmentPrepared(ConfigurableBootstrapContext bootstrapContext, ConfigurableEnvironment environment) {
+        environment.getPropertySources().addLast(new InstalationKitPropertySource());
+    }
+
+    private static class InstalationKitPropertySource extends PropertySource<String> {
+        private static final Map<String, String> PROPS = ImmutableMap.<String, String>builder()
+                .put("szprop", "szvalue")
+                .build() ;
+
+
+        public InstalationKitPropertySource() {
+            super("installation-kit-property-source");
+        }
+
+        @Override
+        public Object getProperty(String name) {
+            return PROPS.get(name);
+        }
+    }
+}
--- learn-java/framework/spring-boot/spring-property/src/main/java/ua/in/sz/pattern/spring/property/Application.java
+++ learn-java/framework/spring-boot/spring-property/src/main/java/ua/in/sz/pattern/spring/property/Application.java
@@ -5,17 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.source.ConfigurationPropertySource;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
 
 @Slf4j
 @Configuration
 @SpringBootApplication
 public class Application implements CommandLineRunner {
-    private final Environment env;
+    private final ConfigurableEnvironment env;
 
     @Autowired
-    public Application(Environment env) {
+    public Application(ConfigurableEnvironment env) {
         this.env = env;
     }
 
@@ -28,5 +32,11 @@ public class Application implements CommandLineRunner {
         log.info("Execute: [{}]", env.getProperty("pattern.text"));
         log.info("Execute build in: [{}]", env.getProperty("pattern.build.in.text"));
         log.info("Execute profile in: [{}]", env.getProperty("pattern.profile.dev.text"));
+        log.info("Execute sz in: [{}]", env.getProperty("szprop"));
+
+        MutablePropertySources propertySources = env.getPropertySources();
+        for (PropertySource<?> propertySource : propertySources) {
+            log.info("Property sources: {}", propertySource);
+        }
     }
 }
