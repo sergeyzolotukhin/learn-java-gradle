41f57206bb5e66db80c9dbb15a01e51d63a385a5
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-02-28 00:29:50 +0200

    java

--- buildSrc/src/main/java/ua/in/sz/plugin/GreetingPlugin.java
+++ buildSrc/src/main/java/ua/in/sz/plugin/GreetingPlugin.java
@@ -2,13 +2,18 @@ package ua.in.sz.plugin;
 
 import org.gradle.api.Project;
 import org.gradle.api.Plugin;
+import org.gradle.api.logging.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public class GreetingPlugin implements Plugin<Project> {
     @Override
     public void apply(@NotNull Project project) {
+        Logger log = project.getLogger();
+
         project.getExtensions().create("greeting", GreetingPluginExtension.class);
         GreetingTask hello = project.getTasks().create("hello", GreetingTask.class);
         hello.setGroup("ge");
+
+        log.lifecycle("=====================");
     }
 }
