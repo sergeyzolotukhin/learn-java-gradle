7638e5c7a2952b4ad5c749b1c1fc61c45929263c
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-03-08 23:56:57 +0200

    exit

--- provision/root-login-enable.sh
+++ provision/root-login-enable.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+cp /vagrant/provision/etc/ssh/ssh_config /etc/ssh/ssh_config
+cp /vagrant/provision/etc/ssh/sshd_config /etc/ssh/sshd_config
+cp /vagrant/provision/etc/ssh/sshd_config.d/60-cloudimg-settings.conf /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 echo -e "root\nroot" | passwd root
 systemctl restart sshd.service
-
-
