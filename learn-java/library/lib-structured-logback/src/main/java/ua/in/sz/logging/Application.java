ec20d71c1bd30c02c645e7965af5ed99b94e575a
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-03-15 21:43:23 +0200

    exit

--- learn-java/library/lib-structured-logback/src/main/java/ua/in/sz/logging/Application.java
+++ learn-java/library/lib-structured-logback/src/main/java/ua/in/sz/logging/Application.java
@@ -7,14 +7,16 @@ import lombok.extern.slf4j.Slf4j;
 public class Application {
 	public static void main(String[] args) {
 		log.atInfo()
-				.setMessage("Error processing given user 1")
+				.setMessage("Error processing given user {}")
+				.addArgument(1)
 				.addKeyValue("key", "key_1")
 				.addKeyValue("exception_class", Application.class.getSimpleName())
 				.addKeyValue("error_message", "ex.getMessage()")
 				.log();
 
 		log.atInfo()
-				.setMessage("Error processing given user 2")
+				.setMessage("Error processing given user {}")
+				.addArgument(2)
 				.addKeyValue("key", "key_2")
 				.addKeyValue("exception_class", Application.class.getSimpleName())
 				.addKeyValue("error_message", "ex.getMessage()")
