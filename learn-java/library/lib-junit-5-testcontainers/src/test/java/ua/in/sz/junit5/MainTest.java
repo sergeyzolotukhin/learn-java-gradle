052d77bcf60d2df846894c9b2c2a29e9366dd5ca
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-03-21 23:42:21 +0200

    exit (spring-security csrf filter basic auth)

--- learn-java/library/lib-junit-5-testcontainers/src/test/java/ua/in/sz/junit5/MainTest.java
+++ learn-java/library/lib-junit-5-testcontainers/src/test/java/ua/in/sz/junit5/MainTest.java
@@ -1,14 +1,16 @@
 package ua.in.sz.junit5;
 
+import io.restassured.RestAssured;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.images.PullPolicy;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 @Slf4j
 @Testcontainers
@@ -16,14 +18,24 @@ class MainTest {
 
     @Container
     public GenericContainer application = new GenericContainer(DockerImageName.parse("szolotukhin/spring-web-rest:0.0.1-SNAPSHOT"))
+            .withImagePullPolicy(PullPolicy.alwaysPull())
             .withExposedPorts(8080);
 
     @Test
     @SneakyThrows
     void main_1() {
-        log.info("main 1 {}", this);
-        TimeUnit.SECONDS.sleep(60);
-        log.info("main 1 {}", this);
+        List<String> messages = RestAssured.given()
+                .baseUri("http://" + application.getHost() + ":" + application.getFirstMappedPort())
+                .contentType("application/json")
+                .when()
+                .get("/api/internal/hello")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .jsonPath().getList(".", String.class);
+
+        log.info("response: {}", messages);
     }
 
 }
