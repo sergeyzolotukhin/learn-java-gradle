a4e1a49bd6eb6fddaa6cf0495556c0e6f331319b
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-04-13 15:44:39 +0300

    exit (spring-security csrf filter basic auth)

--- learn-java/library/lib-apache-exec/src/main/java/ua/in/sz/jgroup/Main.java
+++ learn-java/library/lib-apache-exec/src/main/java/ua/in/sz/jgroup/Main.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 
+import java.io.File;
+
 @Slf4j
 public class Main {
 
@@ -19,5 +21,18 @@ public class Main {
         int exitCode = executor.execute(cmd);
 
         log.info("Command executed: {} with exit code: {}", cmd, exitCode);
+    }
+
+    private static void executeClassOutsideAjar() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+        String classpathEnv = System.getenv("CLASSPATH");
+        log.info("classpathEnv: {}", classpathEnv);
+
+        String classpath = System.getProperty("java.class.path");
+        String[] classpathEntries = classpath.split(File.pathSeparator);
+        for (String classpathEntry : classpathEntries) {
+            log.info("Classpath entry: {}", classpathEntry);
+        }
+
+        Object newObject = Class.forName("b.DebugSupport").newInstance();
     }
 }
