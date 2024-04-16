0bb48dcb7b53dd1ad5530898855eb553aab8f912
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-04-16 19:21:10 +0300

    exit (spring-security csrf filter basic auth)

---
+++ learn-java/library/lib-apache-collections/src/main/java/ua/in/sz/ToString.java
@@ -0,0 +1,28 @@
+package ua.in.sz;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.util.Objects;
+
+@Slf4j
+public class ToString {
+    public static void main(String[] args) {
+        Main m1 = new Main("m1");
+        log.info("to string apache: {}", ToStringBuilder.reflectionToString(m1));
+        log.info("to string: {}", m1);
+        log.info("system to string: {}", Objects.toIdentityString(m1));
+    }
+
+    @AllArgsConstructor
+    private static final class Main {
+        String name;
+
+        @Override
+        public int hashCode() {
+            return 1;
+        }
+    }
+}
