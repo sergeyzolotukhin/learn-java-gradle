4c35ebb3d9cc9b87c66d5f6766f0004418996b7a
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-05-13 18:45:06 +0300

    MapStruct

---
+++ learn-java/core/java-comparable/README.md
@@ -0,0 +1,4 @@
+### Hidden Contract - If two objects are considered equal by the equals operation, then try to make those objects must equal by the Comparable or Comparator test.
+
+https://dzone.com/articles/the-hidden-contract-between-equals-and-comparable
+https://stackoverflow.com/questions/7229977/why-it-is-implied-that-objects-are-equal-if-compareto-returns-0
---
+++ learn-java/core/java-comparable/src/main/java/ua/in/sz/swing/ApplicationRecord.java
@@ -0,0 +1,31 @@
+package ua.in.sz.swing;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+@Slf4j
+public class ApplicationRecord {
+
+    public static void main(String[] args) {
+        Glass plastic = new Glass(Glass.Size.BIG,"Plastic");
+        Glass glass = new Glass(Glass.Size.BIG,"glass");
+
+        // hash set
+        Set<Glass> set = new HashSet<>();
+        set.add(plastic);
+        set.add(glass);
+
+        log.info("HashSet: {}", set);
+
+        // tree set
+        Set<Glass> treeSet = new TreeSet<>();
+        treeSet.add(plastic);
+        treeSet.add(glass);
+
+        log.info("TreeSet: {}", treeSet);
+    }
+}
