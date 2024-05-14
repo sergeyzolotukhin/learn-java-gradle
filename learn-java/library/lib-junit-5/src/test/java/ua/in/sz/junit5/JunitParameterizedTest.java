9319ea64ba7e6b7ad4e2f0e748490b83601c9203
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-05-15 00:06:54 +0300

    MapStruct

---
+++ learn-java/library/lib-junit-5/src/test/java/ua/in/sz/junit5/JunitParameterizedTest.java
@@ -0,0 +1,16 @@
+package ua.in.sz.junit5;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+// https://youtrack.jetbrains.com/issue/IDEA-233324/Cannot-run-individual-JUnit-5-vintage-parametrized-test
+// https://github.com/gradle/gradle/issues/19897
+@Slf4j
+public class JunitParameterizedTest {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, -3, 15, Integer.MAX_VALUE})
+    void isOdd_ShouldReturnTrueForOddNumbers(int number) {
+        log.info("Hello {}", number);
+    }
+}
