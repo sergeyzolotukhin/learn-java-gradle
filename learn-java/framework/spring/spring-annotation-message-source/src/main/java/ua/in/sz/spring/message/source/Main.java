3078eba150e365f632b0f935959ca09e0be77237
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-06-12 22:30:54 +0300

    MapStruct

--- learn-java/framework/spring/spring-annotation-message-source/src/main/java/ua/in/sz/spring/message/source/Main.java
+++ learn-java/framework/spring/spring-annotation-message-source/src/main/java/ua/in/sz/spring/message/source/Main.java
@@ -1,21 +1,29 @@
 package ua.in.sz.spring.message.source;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Main {
+
+    @SneakyThrows
     public static void main(String[] args) {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("classpath:messages");
-        messageSource.setCacheSeconds(10);
+        messageSource.setCacheSeconds(1);
+
+        for (int i = 0; i < 60; i++) {
+            String message1 = messageSource.getMessage("test", new Object[0], Locale.getDefault());
+            log.info("message: {}", message1);
+
+            TimeUnit.MILLISECONDS.sleep(500);
+        }
 
         String message = messageSource.getMessage("test", new Object[0], Locale.of("el"));
         log.info("message: {}", message);
-
-        String message1 = messageSource.getMessage("test", new Object[0], Locale.getDefault());
-        log.info("message: {}", message1);
     }
 }
