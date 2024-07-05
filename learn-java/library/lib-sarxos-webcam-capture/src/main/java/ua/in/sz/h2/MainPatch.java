f335d954a83080376d34b6c42788f44a094903ad
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-07-05 21:08:56 +0300

    JGit

--- learn-java/library/lib-sarxos-webcam-capture/src/main/java/ua/in/sz/h2/MainPatch.java
+++ learn-java/library/lib-sarxos-webcam-capture/src/main/java/ua/in/sz/h2/MainPatch.java
@@ -20,7 +20,8 @@ public class MainPatch {
         }
 
         Webcam webcam = Webcam.getWebcamByName("A4tech FHD 1080P PC Camera 1");
-        webcam.setViewSize(new Dimension(640, 480));
+        webcam.setCustomViewSizes(new Dimension(1920, 1080));
+        webcam.setViewSize(new Dimension(1920, 1080));
         webcam.open();
         ImageIO.write(webcam.getImage(), "PNG", new File("hello-world.png"));
 
