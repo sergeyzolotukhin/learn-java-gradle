1b873802e330c5df29ed5c374aa52d24f67992c1
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-07-05 21:55:54 +0300

    QR code

---
+++ learn-java/library/lib-zxing/README.txt
@@ -0,0 +1,3 @@
+https://www.geeksforgeeks.org/how-to-generate-and-read-qr-code-with-java-using-zxing-library/
+
+https://github.com/zxing/zxing/blob/master/javase/src/main/java/com/google/zxing/client/j2se/CommandLineEncoder.java
---
+++ learn-java/library/lib-zxing/src/main/java/ua/in/sz/h2/MainPatch.java
@@ -0,0 +1,12 @@
+package ua.in.sz.h2;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MainPatch {
+    public static void main(String[] args) throws Exception {
+        log.info("Starting Git");
+
+        log.info("Ending Git");
+    }
+}
