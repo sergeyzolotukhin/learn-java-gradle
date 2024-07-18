e4d08a9312351f366e1d5ed7822027d03da18c15
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-07-18 22:10:44 +0300

    QR code

--- learn-java/library/lib-junit-5/src/test/java/ua/in/sz/junit5/MyExecutionListener.java
+++ learn-java/library/lib-junit-5/src/test/java/ua/in/sz/junit5/MyExecutionListener.java
@@ -1,18 +1,30 @@
 package ua.in.sz.junit5;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.launcher.TestExecutionListener;
+import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
 
 @Slf4j
 public class MyExecutionListener implements TestExecutionListener {
     @Override
     public void testPlanExecutionStarted(TestPlan testPlan) {
-        log.debug("Executing testPlan: {}", testPlan);
+        log.info("Executing testPlan: {}", testPlan);
     }
 
     @Override
     public void testPlanExecutionFinished(TestPlan testPlan) {
-        log.debug("Finished testPlan: {}", testPlan);
+        log.info("Finished testPlan: {}", testPlan);
+    }
+
+    @Override
+    public void executionStarted(TestIdentifier testIdentifier) {
+        log.info("Executing testIdentifier: {}", testIdentifier);
+    }
+
+    @Override
+    public void executionFinished(TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) {
+        log.info("Finished testIdentifier: {}", testIdentifier);
     }
 }
