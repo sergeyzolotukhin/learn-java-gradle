cdfe5a1721ae7716188ef2d70f4ff23b3516a5fb
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-09-17 22:51:13 +0300

    dbunit tests

--- learn-java/framework/spring-boot/spring-hibernate-thread/src/main/java/ua/in/sz/spring/initialize/InitializeApplication.java
+++ learn-java/framework/spring-boot/spring-hibernate-thread/src/main/java/ua/in/sz/spring/initialize/InitializeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 @SpringBootApplication
 public class InitializeApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(InitializeApplication.class, args);
 	}
