a2b22fbdbc367cfeedef49f288c341b2bf10ee8d
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-09-19 17:40:04 +0300

    dbunit tests

--- learn-java/core/task-executor/src/main/java/ua/in/sz/h2/ThreadPoolExecutorAfterExecuteMain.java
+++ learn-java/core/task-executor/src/main/java/ua/in/sz/h2/ThreadPoolExecutorAfterExecuteMain.java
@@ -1,8 +1,12 @@
 package ua.in.sz.h2;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -11,27 +15,42 @@ public class ThreadPoolExecutorAfterExecuteMain {
     public static void main(String[] args) {
         log.info("Start");
 
-        try (ThreadPoolExecutor executor = new ThreadPoolExecutor(
-                1, 2,
-                10, TimeUnit.SECONDS,
+        try (ThreadPoolExecutor executor = new LoggedThreadPoolExecutor(
+                2, 2,
+                100, TimeUnit.MILLISECONDS,
                 new ArrayBlockingQueue<>(20)
         )) {
 
             for (int i = 0; i < 4; i++) {
                 executor.submit(new Runnable() {
                     @Override
+                    @SneakyThrows
                     public void run() {
+                        TimeUnit.MILLISECONDS.sleep(1000);
                         log.info("Run");
                     }
                 });
             }
 
-            executor.awaitTermination(10, TimeUnit.SECONDS);
             executor.shutdown();
+            executor.awaitTermination(10, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
         log.info("End");
+    }
+
+    public static class LoggedThreadPoolExecutor extends ThreadPoolExecutor {
+
+        public LoggedThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+        }
+
+        @Override
+        protected void afterExecute(Runnable r, Throwable t) {
+            super.afterExecute(r, t);
+            log.info("After execute");
+        }
     }
 }
