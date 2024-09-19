1922439a431d99b482cbf59fc96380d430f95290
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-09-19 19:44:21 +0300

    dbunit tests

---
+++ learn-java/framework/spring/spring-xml-minimal/src/test/java/ua/in/sz/h2/MainTest.java
@@ -0,0 +1,53 @@
+package ua.in.sz.h2;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MainTest {
+
+    @Test
+    void main() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(10);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        executor.setTaskDecorator(runnable -> () -> {
+            log.info("before");
+            runnable.run();
+            log.info("after");
+        });
+
+
+        executor.submit(() -> {
+            log.info("run");
+            sleet(100);
+        });
+
+        for (int i = 0; i < 5; i++) {
+            executor.submit(() -> {
+                log.info("run");
+                sleet(100);
+            });
+        }
+
+        sleet(3000);
+//        executor.initiateShutdown();
+        executor.shutdown();
+
+    }
+
+    @SneakyThrows
+    private static void sleet(int timeout) {
+        TimeUnit.MILLISECONDS.sleep(timeout);
+    }
+}
