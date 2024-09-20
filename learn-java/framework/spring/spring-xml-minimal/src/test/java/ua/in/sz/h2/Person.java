7ec8165e20403ca908e7aebe3e5f7abe1184ca13
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-09-20 22:03:17 +0300

    dbunit tests

--- learn-java/framework/spring/spring-xml-minimal/src/test/java/ua/in/sz/h2/Person.java
+++ learn-java/framework/spring/spring-xml-minimal/src/test/java/ua/in/sz/h2/Person.java
@@ -2,10 +2,15 @@ package ua.in.sz.h2;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Setter
 @Getter
 public class Person {
@@ -19,5 +24,39 @@ public class Person {
 
     public void setGender(String gender) {
 
+    }
+
+    @SuppressWarnings("all")
+    public List<String> getDescriptions() {
+        log.info("getDescriptions()");
+        List<String> proxyInstance = (List<String>) Proxy.newProxyInstance(
+                Person.class.getClassLoader(),
+                new Class[] { List.class },
+                new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        if (method.getName().equals("add")) {
+                            return descriptions.add((String) args[0]);
+                        }
+
+                        if (method.getName().equals("toString")) {
+                            return descriptions.toString();
+                        }
+
+                        throw new UnsupportedOperationException(method.getName());
+//                        if (method.getName().equals("add")) {
+//
+//                        }
+//                        return descriptions;
+
+//                        log.info("invoke method {}", args);
+                    }
+                });
+
+        return proxyInstance;
+    }
+
+    public void setDescriptions(List<String> descriptions) {
+        this.descriptions = descriptions;
     }
 }
