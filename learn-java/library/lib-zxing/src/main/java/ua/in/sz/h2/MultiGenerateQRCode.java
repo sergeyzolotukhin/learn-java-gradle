6d3526eb4485d380a9ac0f232d94fcecb187a122
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-10-05 19:40:25 +0300

    dbunit tests

---
+++ learn-java/library/lib-zxing/src/main/java/ua/in/sz/h2/MultiGenerateQRCode.java
@@ -0,0 +1,62 @@
+package ua.in.sz.h2;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+@Slf4j
+public class MultiGenerateQRCode {
+    public static void main(String[] args) throws WriterException, IOException {
+        for (int i = 0; i < 10; i++) {
+            String qrCodeText = String.format("https://www.journaldev.com %08d", i);
+            String filePath = String.format("learn-java/library/lib-zxing/JD-%08d.png", i);
+            int size = 125;
+            String fileType = "png";
+            File qrFile = new File(filePath);
+            createQRImage(qrFile, qrCodeText, size, fileType);
+        }
+
+        log.info("DONE");
+    }
+
+    private static void createQRImage(File qrFile, String qrCodeText, int size, String fileType) throws WriterException, IOException {
+        // Create the ByteMatrix for the QR-Code that encodes the given String
+        Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<>();
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix byteMatrix = qrCodeWriter.encode(qrCodeText, BarcodeFormat.QR_CODE, size, size, hintMap);
+
+
+        // Paint and save the image using the ByteMatrix
+        int matrixWidth = byteMatrix.getWidth();
+        BufferedImage image = new BufferedImage(matrixWidth, matrixWidth, BufferedImage.TYPE_INT_RGB);
+        image.createGraphics();
+
+        Graphics2D graphics = (Graphics2D) image.getGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, matrixWidth, matrixWidth);
+
+        graphics.setColor(Color.BLACK);
+
+        for (int i = 0; i < matrixWidth; i++) {
+            for (int j = 0; j < matrixWidth; j++) {
+                if (byteMatrix.get(i, j)) {
+                    graphics.fillRect(i, j, 1, 1);
+                }
+            }
+        }
+        ImageIO.write(image, fileType, qrFile);
+    }
+}
