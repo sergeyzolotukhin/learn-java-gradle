108933a06c380a3edc62da7f93c2f09fc83c1f09
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-10-07 19:44:20 +0300

    dbunit tests

--- learn-java/library/lib-tesseract/src/main/java/ua/in/sz/jgroup/Main.java
+++ learn-java/library/lib-tesseract/src/main/java/ua/in/sz/jgroup/Main.java
@@ -10,7 +10,7 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        File imageFile = new File("j:/_capute/RECentral/2024100511233621.png");
+        File imageFile = new File("j:\\_capute\\ge\\2024100718320771.mp4_snapshot_00.50.049.png");
 //        File imageFile = new File("D:/projects-java/_learn-java-gradle/learn-java/library/lib-tesseract/src/main/resources/2024100511233621.png");
         ITesseract instance = new Tesseract();
         instance.setDatapath("D:/projects-java/_learn-java-gradle/learn-java/library/lib-tesseract/src/main/resources");
