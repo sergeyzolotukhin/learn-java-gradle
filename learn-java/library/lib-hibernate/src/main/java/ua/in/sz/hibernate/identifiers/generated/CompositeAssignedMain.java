9513b6dccd4f9d1705b82a2d4e42736f7e8e76b3
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-10-10 22:19:02 +0300

    dbunit tests

---
+++ learn-java/library/lib-hibernate/src/main/java/ua/in/sz/hibernate/identifiers/generated/CompositeAssignedMain.java
@@ -0,0 +1,48 @@
+package ua.in.sz.hibernate.identifiers.generated;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ua.in.sz.hibernate.identifiers.generated.entities.CompositeAssignedDerivation;
+
+@Slf4j
+public class CompositeAssignedMain {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().build();
+        try (
+                SessionFactory sessionFactory = new MetadataSources(registry)
+                        .addAnnotatedClass(CompositeAssignedDerivation.class)
+                        .buildMetadata()
+                        .buildSessionFactory()
+        ) {
+            insertDerivation(sessionFactory);
+        } catch (Exception e) {
+            log.error("Error: ", e);
+            // The registry would be destroyed by the SessionFactory, but we
+            // had trouble building the SessionFactory so destroy it manually.
+            StandardServiceRegistryBuilder.destroy(registry);
+
+        }
+    }
+
+    private static void insertDerivation(SessionFactory sessionFactory) {
+        Session em = sessionFactory.openSession();
+
+        em.getTransaction().begin();
+
+        for (int i = 0; i < 20; i++) {
+            CompositeAssignedDerivation derivation = CompositeAssignedDerivation.builder()
+                    .id(2L)
+                    .name(String.format("Derivation %s", i))
+                    .build();
+            em.persist(derivation);
+            log.info("persist derivation : {} / {}", derivation.getId(), derivation.getSubId());
+        }
+
+        em.getTransaction().commit();
+        em.clear();
+    }
+}
---
+++ learn-java/library/lib-hibernate/src/main/java/ua/in/sz/hibernate/identifiers/generated/entities/CompositeAssignedDerivation.java
@@ -0,0 +1,42 @@
+package ua.in.sz.hibernate.identifiers.generated.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldNameConstants;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldNameConstants
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "DERIVATION")
+public class CompositeAssignedDerivation {
+
+	@Id
+	@Column(name = "ID")
+	private Long id;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employee_generator_02")
+	@SequenceGenerator(name = "employee_generator_02", sequenceName = "employee_seq_02", allocationSize = 1)
+	@Column(name = "SUB_ID")
+	private Long subId;
+
+	@Column(name = "NAME")
+	@EqualsAndHashCode.Include
+	private String name;
+}
