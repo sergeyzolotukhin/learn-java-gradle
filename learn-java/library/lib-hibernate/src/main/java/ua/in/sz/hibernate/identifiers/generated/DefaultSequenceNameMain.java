040e5899bb62c92d238780793f602d8add071522
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-10-10 22:26:06 +0300

    dbunit tests

---
+++ learn-java/library/lib-hibernate/src/main/java/ua/in/sz/hibernate/identifiers/generated/DefaultSequenceNameMain.java
@@ -0,0 +1,53 @@
+package ua.in.sz.hibernate.identifiers.generated;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ua.in.sz.hibernate.identifiers.generated.entities.DefaultSequenceNameDerivation;
+import ua.in.sz.hibernate.identifiers.generated.entities.DefaultSequenceNameSecondDerivation;
+
+@Slf4j
+public class DefaultSequenceNameMain {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().build();
+        try (
+                SessionFactory sessionFactory = new MetadataSources(registry)
+                        .addAnnotatedClass(DefaultSequenceNameDerivation.class)
+                        .addAnnotatedClass(DefaultSequenceNameSecondDerivation.class)
+                        .buildMetadata()
+                        .buildSessionFactory()
+        ) {
+            insertDerivation(sessionFactory);
+        } catch (Exception e) {
+            log.error("Error: ", e);
+            // The registry would be destroyed by the SessionFactory, but we
+            // had trouble building the SessionFactory so destroy it manually.
+            StandardServiceRegistryBuilder.destroy(registry);
+
+        }
+    }
+
+    private static void insertDerivation(SessionFactory sessionFactory) {
+        Session em = sessionFactory.openSession();
+
+        em.getTransaction().begin();
+
+        for (int i = 0; i < 20; i++) {
+            DefaultSequenceNameDerivation derivation = DefaultSequenceNameDerivation.builder()
+                    .name(String.format("Derivation %s", i))
+                    .build();
+            em.persist(derivation);
+
+            DefaultSequenceNameSecondDerivation secondDerivation = DefaultSequenceNameSecondDerivation.builder()
+                    .name(String.format("Derivation %s", i))
+                    .build();
+            em.persist(secondDerivation);
+        }
+
+        em.getTransaction().rollback();
+        em.clear();
+    }
+}
---
+++ learn-java/library/lib-hibernate/src/main/java/ua/in/sz/hibernate/identifiers/generated/entities/DefaultSequenceNameSecondDerivation.java
@@ -0,0 +1,34 @@
+package ua.in.sz.hibernate.identifiers.generated.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldNameConstants;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldNameConstants
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "SECOND_DERIVATION")
+public class DefaultSequenceNameSecondDerivation {
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "NAME")
+	@EqualsAndHashCode.Include
+	private String name;
+}
