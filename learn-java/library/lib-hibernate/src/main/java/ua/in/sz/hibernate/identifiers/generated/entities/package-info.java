d21f41071e33bea5b576ffa7acf99246fdbe1063
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-10-15 19:31:40 +0300

    dbunit tests

---
+++ learn-java/library/lib-hibernate/src/main/java/ua/in/sz/hibernate/identifiers/generated/entities/package-info.java
@@ -0,0 +1,13 @@
+//@GenericGenerator(
+//        name = "DefaultGenerator",
+//        strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+//        parameters = {
+//                @Parameter(name = "optimizer", value = "pooled"),
+//                @Parameter(name = "increment_size", value = "1000")
+//        }
+//)
+package ua.in.sz.hibernate.identifiers.generated.entities;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
