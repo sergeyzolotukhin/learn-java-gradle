18ecce52cd96ed46bfb2e028b83438da30ee2974
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-10-19 17:54:25 +0300

    dbunit tests

---
+++ proof-of-concept/saboteur/src/main/java/ua/in/sz/saboteur/SaboteurMain.java
@@ -0,0 +1,12 @@
+package ua.in.sz.saboteur;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SaboteurMain {
+    @SneakyThrows
+    public static void main(String[] args) {
+        log.info("Hello World!");
+    }
+}
