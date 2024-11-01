f9ad15252a63216bc55da59a6f76aef394e52847
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-11-01 22:30:54 +0200

    dbunit tests

--- learn-java/library/lib-bean-validation/src/main/java/ua/in/sz/bean/validation/ValidationApplication.java
+++ learn-java/library/lib-bean-validation/src/main/java/ua/in/sz/bean/validation/ValidationApplication.java
@@ -12,13 +12,13 @@ import java.util.Set;
 @Slf4j
 public class ValidationApplication {
 	public static void main(String[] args) {
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-		Validator validator = factory.getValidator();
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+			Validator validator = factory.getValidator();
+			Rect rect = Rect.builder().x(-10).y(-15).height(2000).width(-1).build();
 
-		Rect rect = Rect.builder().x(-10).y(-15).height(2000).width(-1).build();
+			Set<ConstraintViolation<Rect>> violations = validator.validate(rect);
 
-		Set<ConstraintViolation<Rect>> violations = validator.validate(rect);
-
-		violations.forEach(v -> log.info("Violation: [{}]", v.getMessage()));
+			violations.forEach(v -> log.info("Violation: [{}]", v.getMessage()));
+        }
 	}
 }
