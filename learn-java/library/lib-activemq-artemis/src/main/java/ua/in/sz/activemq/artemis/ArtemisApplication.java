013bab66c89d5d58c2689934b138c9d65f91681d
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-11-19 23:08:23 +0200

    commit

--- learn-java/library/lib-activemq-artemis/src/main/java/ua/in/sz/activemq/artemis/ArtemisApplication.java
+++ learn-java/library/lib-activemq-artemis/src/main/java/ua/in/sz/activemq/artemis/ArtemisApplication.java
@@ -5,37 +5,37 @@ import org.apache.activemq.artemis.api.core.TransportConfiguration;
 import org.apache.activemq.artemis.api.jms.ActiveMQJMSClient;
 import org.apache.activemq.artemis.api.jms.JMSFactoryType;
 import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory;
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
 import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
-@SuppressWarnings("ALL")
 @Slf4j
 public class ArtemisApplication {
     public static void main(String[] args) throws Exception {
         TransportConfiguration transportConfiguration = new TransportConfiguration(NettyConnectorFactory.class.getName());
 
-        ConnectionFactory cf = ActiveMQJMSClient.createConnectionFactoryWithoutHA(JMSFactoryType.CF, transportConfiguration);
+        try (ActiveMQConnectionFactory cf = ActiveMQJMSClient.createConnectionFactoryWithoutHA(JMSFactoryType.CF, transportConfiguration)) {
+            try (Connection connection = cf.createConnection("admin", "admin")) {
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-        Queue orderQueue = ActiveMQJMSClient.createQueue("OrderQueue");
+                Queue orderQueue = session.createQueue("OrderQueue");
 
-        Connection connection = cf.createConnection("admin", "admin");
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                MessageProducer producer = session.createProducer(orderQueue);
+                MessageConsumer consumer = session.createConsumer(orderQueue);
 
-        MessageProducer producer = session.createProducer(orderQueue);
-        MessageConsumer consumer = session.createConsumer(orderQueue);
+                connection.start();
 
-        connection.start();
+                TextMessage message = session.createTextMessage("This is an order");
+                producer.send(message);
 
-        TextMessage message = session.createTextMessage("This is an order");
-        producer.send(message);
-
-        TextMessage receivedMessage = (TextMessage) consumer.receive();
-        log.info("Got order: {}", receivedMessage.getText());
+                TextMessage receivedMessage = (TextMessage) consumer.receive();
+                log.info("Got order: {}", receivedMessage.getText());
+            }
+        }
     }
 }
