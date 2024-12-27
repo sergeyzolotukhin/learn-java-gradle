fec6748015a5b818aafc634590c0c426ac4a7946
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-12-27 19:08:31 +0200

    commit

--- learn-java/core/task-executor/src/main/java/ua/in/sz/executor/service/future/CompletableFutureThenCombine.java
+++ learn-java/core/task-executor/src/main/java/ua/in/sz/executor/service/future/CompletableFutureThenCombine.java
@@ -1,10 +1,12 @@
 package ua.in.sz.executor.service.future;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class CompletableFutureThenCombine {
@@ -12,14 +14,18 @@ public class CompletableFutureThenCombine {
         try (ExecutorService executor = Executors.newFixedThreadPool(10)) {
             CompletableFuture<String> future1 = CompletableFuture.supplyAsync(
                     () -> {
-                        log.info("Educative");
+                        log.info("Educative starting");
+                        sleep(2);
+                        log.info("Educative ended");
                         return "Educative";
                     },
                     executor);
 
             CompletableFuture<String> future2 = CompletableFuture.supplyAsync(
                     () -> {
-                        log.info("Edpresso");
+                        log.info("Edpresso starting");
+                        sleep(3);
+                        log.info("Edpresso ended");
                         return "Edpresso";
                     },
                     executor);
@@ -27,12 +33,19 @@ public class CompletableFutureThenCombine {
             CompletableFuture<String> future = future1.thenCombineAsync(future2,
                     (s1, s2) -> {
                         String result = s1 + " : " + s2;
-                        log.info("thenCombine: {}", result);
+                        log.info("thenCombine starting: {}", result);
+                        sleep(3);
+                        log.info("thenCombine ended: {}", result);
                         return result;
                     },
                     executor);
 
             log.info("Future 1: {}", future.join());
         }
+    }
+
+    @SneakyThrows
+    private static void sleep(long timeout) {
+        TimeUnit.SECONDS.sleep(timeout);
     }
 }
