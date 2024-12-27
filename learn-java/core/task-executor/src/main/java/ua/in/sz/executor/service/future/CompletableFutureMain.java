392c50b9a698b5044453cb5dec431156efd11310
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-12-27 17:33:12 +0200

    commit

--- learn-java/core/task-executor/src/main/java/ua/in/sz/executor/service/future/CompletableFutureMain.java
+++ learn-java/core/task-executor/src/main/java/ua/in/sz/executor/service/future/CompletableFutureMain.java
@@ -1,11 +1,13 @@
 package ua.in.sz.executor.service.future;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 @Slf4j
@@ -13,19 +15,30 @@ public class CompletableFutureMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         log.info("Starting");
 
-        try (ExecutorService executor = Executors.newSingleThreadExecutor()) {
-            CompletableFuture<String> future = CompletableFuture.supplyAsync(CompletableFutureMain::getSupplyAsync, executor);
+        try (ExecutorService executor = Executors.newFixedThreadPool(10)) {
+            CompletableFuture<String> future1 = CompletableFuture.supplyAsync(new MySupplier("Task 1", 1), executor);
+            CompletableFuture<String> future2 = CompletableFuture.supplyAsync(new MySupplier("Task 2", 2), executor);
+            CompletableFuture<String> future3 = CompletableFuture.supplyAsync(new MySupplier("Task 3", 3), executor);
             log.info("Submitted");
+            CompletableFuture<Void> allOf = CompletableFuture.allOf(future1, future2, future3);
 
-            log.info("Result: [{}]", future.get());
+            log.info("Result: [{}]", allOf.get());
         }
 
         log.info("Ended");
-
     }
 
-    private static String getSupplyAsync() {
-        log.info("SupplyAsync");
-        return "Hello World";
+    @Slf4j
+    private record MySupplier(String name, int timeout) implements Supplier<String> {
+        @SneakyThrows
+        @Override
+        public String get() {
+            log.info("starting: [{}]", name);
+
+            TimeUnit.SECONDS.sleep(timeout);
+
+            log.info("ended: [{}]", name);
+            return "Hello " + name;
+        }
     }
 }
