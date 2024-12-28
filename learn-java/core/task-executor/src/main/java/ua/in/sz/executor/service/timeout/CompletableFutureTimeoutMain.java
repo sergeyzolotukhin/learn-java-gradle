137c594bc7f988fcf7187765818c58093a045ce8
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-12-28 13:00:04 +0200

    commit

--- learn-java/core/task-executor/src/main/java/ua/in/sz/executor/service/timeout/CompletableFutureTimeoutMain.java
+++ learn-java/core/task-executor/src/main/java/ua/in/sz/executor/service/timeout/CompletableFutureTimeoutMain.java
@@ -3,14 +3,11 @@ package ua.in.sz.executor.service.timeout;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.function.Supplier;
 
 /**
