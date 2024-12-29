4e96045a800b83caf3b869eec88533318d4dba59
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-12-29 13:41:32 +0200

    commit

--- learn-java/core/task-executor/src/main/java/ua/in/sz/executor/service/future/CompletableFutureThenCompose.java
+++ learn-java/core/task-executor/src/main/java/ua/in/sz/executor/service/future/CompletableFutureThenCompose.java
@@ -4,37 +4,60 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @Slf4j
 public class CompletableFutureThenCompose {
-    public static void main(String[] args) {
-        try (ExecutorService executor = Executors.newFixedThreadPool(10)) {
-            CompletableFuture<String> future1 = CompletableFuture.supplyAsync(
-                    () -> {
-                        log.info("Educative starting");
-                        sleep(2);
-                        log.info("Educative ended");
-                        return "Educative";
-                    },
-                    executor);
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        log.info("Starting app");
 
-            CompletableFuture<String> future = future1.thenComposeAsync(
+        try (ExecutorService executor = Executors.newSingleThreadExecutor()) {
+            CompletableFuture<String> future1 = CompletableFuture.supplyAsync(new FirstTask(), executor);
+
+            CompletableFuture<String> future2 = future1.thenComposeAsync(
                     (Function<String, CompletableFuture<String>>) s -> CompletableFuture.supplyAsync(
-                            () -> {
-                                String result = "Food Served" + s;
-                                log.info("Starting: {}", result);
-                                sleep(3);
-                                log.info("Ended: {}", result);
-                                return result;
-                            },
+                            new SecondTask(s),
                             executor),
                     executor);
 
-            log.info("result: {}", future.join());
+            log.info("Submitted");
+
+//            sleep(6);
+            String result = future2.get();
+
+            log.info("Done {}, Canceled {}, state {}, result {}",
+                    future2.isDone(), future2.isCancelled(), future2.state(), result);
+
+        }
+
+        log.info("Ended app");
+    }
+
+    static class FirstTask implements Supplier<String> {
+        @Override
+        @SneakyThrows
+        public String get() {
+            log.info("Starting FirstTask");
+            sleep(2);
+            log.info("Ended FirstTask");
+            return "FirstTask";
+        }
+    }
+
+    static record SecondTask(String s) implements Supplier<String> {
+        @Override
+        @SneakyThrows
+        public String get() {
+            String result = "SecondTask " + s;
+            log.info("Starting: {}", result);
+            sleep(2);
+            log.info("Ended: {}", result);
+            return result;
         }
     }
 
