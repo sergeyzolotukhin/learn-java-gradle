b0eef3d363888fc6ea4d5054de89cc4287255dca
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-01-28 20:13:20 +0200

    commit

---
+++ learn-java/framework/spring-boot/spring-boot-configuration-metadata/README.md
@@ -0,0 +1,6 @@
+#### Spring Externalized Configuration
+https://docs.spring.io/spring-boot/reference/features/external-config.html
+
+#### Configuration Metadata
+META-INF/spring-configuration-metadata.json
+https://docs.spring.io/spring-boot/specification/configuration-metadata/annotation-processor.html
--- learn-java/framework/spring-boot/spring-boot-configuration-metadata/src/main/java/ua/in/sz/pattern/spring/yaml/YamlApplication.java
+++ learn-java/framework/spring-boot/spring-boot-configuration-metadata/src/main/java/ua/in/sz/pattern/spring/yaml/YamlApplication.java
@@ -1,10 +1,6 @@
 package ua.in.sz.pattern.spring.yaml;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -13,8 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
-import org.springframework.core.env.Environment;
-import ua.in.sz.pattern.spring.property.BusinessService;
 
 // https://docs.spring.io/spring-boot/docs/2.1.7.RELEASE/reference/html/boot-features-external-config.html
 // https://www.baeldung.com/spring-yaml-propertysource
@@ -24,7 +18,7 @@ import ua.in.sz.pattern.spring.property.BusinessService;
 @SpringBootApplication
 @EnableConfigurationProperties(AcmeProperties.class)
 @PropertySources({
-        @PropertySource(value = "classpath:rules.yml", factory = YamlPropertySourceFactory.class)
+        @PropertySource(value = "classpath:application.yml", factory = YamlPropertySourceFactory.class)
 })
 public class YamlApplication {
     public static void main(String[] args) {
