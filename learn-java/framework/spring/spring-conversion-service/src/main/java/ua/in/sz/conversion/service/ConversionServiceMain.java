2963052c4fc9518c9b95a8c10d88ce98b022ea5c
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-02-05 20:03:31 +0200

    commit

--- learn-java/framework/spring/spring-conversion-service/src/main/java/ua/in/sz/conversion/service/ConversionServiceMain.java
+++ learn-java/framework/spring/spring-conversion-service/src/main/java/ua/in/sz/conversion/service/ConversionServiceMain.java
@@ -5,10 +5,9 @@ import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.support.ConfigurableConversionService;
 
 @Slf4j
 @Configuration
@@ -18,24 +17,23 @@ public class ConversionServiceMain {
         context.scan("ua.in.sz.conversion.service");
         context.refresh();
 
-        ConversionService conversionService = context.getBean(ConversionService.class);
+        ConfigurableConversionService conversionService = context.getEnvironment().getConversionService();
+        conversionService.addConverter(new StringToMyDtoConverter());
 
         MyDto value = conversionService.convert("123:321", MyDto.class);
+        log.info("{}", value);
 
         context.close();
-
-        log.info("{}", value);
     }
 
-    @Bean
-    public ConversionService conversionService() {
-        DefaultConversionService service = new DefaultConversionService();
-        service.addConverter(String.class, MyDto.class,
-                source -> MyDto.builder()
-                        .value(Integer.parseInt(source.split(":")[0]))
-                        .precision(Integer.parseInt(source.split(":")[1]))
-                        .origen(source).build());
-        return service;
+    public static class StringToMyDtoConverter implements Converter<String, MyDto> {
+        @Override
+        public MyDto convert(String source) {
+            return MyDto.builder()
+                    .value(Integer.parseInt(source.split(":")[0]))
+                    .precision(Integer.parseInt(source.split(":")[1]))
+                    .origen(source).build();
+        }
     }
 
     @ToString
