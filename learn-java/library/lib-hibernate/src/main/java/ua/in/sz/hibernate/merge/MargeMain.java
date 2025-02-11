a79564f340907052379c82057580b408cdda1c6c
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-02-11 22:40:01 +0200

    commit

---
+++ learn-java/library/lib-hibernate/src/main/java/ua/in/sz/hibernate/merge/MargeMain.java
@@ -0,0 +1,71 @@
+package ua.in.sz.hibernate.merge;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ua.in.sz.hibernate.merge.entities.Group;
+
+import java.util.List;
+
+@Slf4j
+public class MargeMain {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().build();
+        try (
+                SessionFactory sessionFactory = new MetadataSources(registry)
+                        .addAnnotatedClass(Group.class)
+                        .buildMetadata()
+                        .buildSessionFactory()
+        ) {
+            insertGroups(sessionFactory);
+            queryGroup(sessionFactory);
+        } catch (Exception e) {
+            log.error("Error: ", e);
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    private static void queryGroup(SessionFactory sessionFactory) {
+        Session s = sessionFactory.openSession();
+        s.getTransaction().begin();
+        Group group = s.byNaturalId(Group.class)
+                .using(Group.Fields.name, "GR_D")
+                .load();
+        log.info("Dep Step 3: {}", group);
+        s.getTransaction().commit();
+        s.close();
+    }
+
+    private static void insertGroups(SessionFactory sessionFactory) {
+        Session em = sessionFactory.openSession();
+
+        // model
+        Group a = Group.builder().name("GR_A").build();
+        Group b = Group.builder().name("GR_B").build();
+        Group c = Group.builder().name("GR_C").build();
+
+        Group d = Group.builder().name("GR_D")
+                .withChild(a)
+                .withChild(b)
+                .withParent(c)
+                .build();
+
+        em.getTransaction().begin();
+
+        em.persist(a);
+        em.persist(b);
+        em.persist(c);
+        em.persist(d);
+
+        log.info("persisted");
+        em.getTransaction().commit();
+        em.clear();
+
+        em.close();
+
+        log.info("commited");
+    }
+}
---
+++ learn-java/library/lib-hibernate/src/main/java/ua/in/sz/hibernate/merge/entities/Group.java
@@ -0,0 +1,79 @@
+package ua.in.sz.hibernate.merge.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldNameConstants;
+import org.hibernate.annotations.NaturalId;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SuppressWarnings("JpaDataSourceORMInspection")
+@FieldNameConstants
+@ToString(onlyExplicitlyIncluded = true, includeFieldNames = false)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "DET_GROUP")
+public class Group {
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private Long id;
+    @ToString.Include
+    @NaturalId
+    @Column(name = "NAME")
+    private String name;
+
+    @OneToMany
+
+    @JoinTable(name = "DEP_GROUP_TO_PARENT",
+            foreignKey = @ForeignKey(name = "FK_PARENT_GR_01"),
+            inverseForeignKey = @ForeignKey(name = "FK_PARENT_GR_2")
+    )
+    @Builder.Default
+    private Set<Group> parents = new HashSet<>();
+    @OneToMany
+    @JoinTable(name = "DEP_GROUP_TO_CHILD",
+            foreignKey = @ForeignKey(name = "FK_CHILD_GR_1"),
+            inverseForeignKey = @ForeignKey(name = "FK_CHILD_GR_2")
+    )
+    @Builder.Default
+    private Set<Group> children = new HashSet<>();
+
+    @SuppressWarnings("unused")
+    public static class GroupBuilder {
+        public GroupBuilder withParent(Group parent) {
+            if (this.parents$value == null) {
+                this.parents$value = new HashSet<>();
+            }
+            this.parents$value.add(parent);
+            this.parents$set = true;
+            return this;
+        }
+
+        public GroupBuilder withChild(Group child) {
+            if (this.children$value == null) {
+                this.children$value = new HashSet<>();
+            }
+            this.children$value.add(child);
+            this.children$set = true;
+            return this;
+        }
+    }
+}
