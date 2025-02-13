44b36ac161ec9bf9a33dd875687e17e61633d3ac
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-02-13 22:01:58 +0200

    commit

--- learn-java/library/lib-jackson/lib-jackson-json/src/main/java/ua/in/szolotukhin/jackson/reference/exists/ReferenceExistsApplication.java
+++ learn-java/library/lib-jackson/lib-jackson-json/src/main/java/ua/in/szolotukhin/jackson/reference/exists/ReferenceExistsApplication.java
@@ -15,16 +15,20 @@ public class ReferenceExistsApplication {
     public static void main(String[] args) throws Exception {
         ObjectMapper mapper = createMapper();
 
-        School school = new School(1, "St Magdalene's");
-        Student mary = new Student(1, "Mary", school);
-        Student bob = new Student(2, "Bob", school);
-        Student[] students = new Student[]{mary, bob};
+        Student[] students = createModel();
 
         String serialized = mapper.writeValueAsString(students);
         log.info("Serialized: {}", serialized);
 
         List<Student> ourStudents = Arrays.asList(mapper.readValue(serialized, Student[].class));
         log.info("Our students: {}", ourStudents);
+    }
+
+    private static Student[] createModel() {
+        School school = new School(1, "St Magdalene's");
+        Student mary = new Student(1, "Mary", school);
+        Student bob = new Student(2, "Bob", school);
+        return new Student[]{mary, bob};
     }
 
     public static ObjectMapper createMapper() {
