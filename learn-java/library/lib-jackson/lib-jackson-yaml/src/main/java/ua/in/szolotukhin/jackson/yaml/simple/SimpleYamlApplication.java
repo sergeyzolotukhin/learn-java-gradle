51d5bfa78772257c7d6aba4c25b5bb7406a917f6
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-02-13 21:11:13 +0200

    commit

--- learn-java/library/lib-jackson/lib-jackson-yaml/src/main/java/ua/in/szolotukhin/jackson/yaml/simple/SimpleYamlApplication.java
+++ learn-java/library/lib-jackson/lib-jackson-yaml/src/main/java/ua/in/szolotukhin/jackson/yaml/simple/SimpleYamlApplication.java
@@ -8,8 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import ua.in.szolotukhin.jackson.yaml.simple.model.Order;
 import ua.in.szolotukhin.jackson.yaml.simple.model.OrderLine;
 
+import java.io.IOException;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -20,6 +20,10 @@ public class SimpleYamlApplication {
     public static void main(String[] args) throws Exception {
         ObjectMapper mapper = createMapper();
 
+        writeYaml(mapper);
+    }
+
+    private static void writeYaml(ObjectMapper mapper) throws IOException {
         List<OrderLine> lines = new ArrayList<>();
         lines.add(new OrderLine("Copper Wire (200ft)", 1, new BigDecimal("50.67")));
         lines.add(new OrderLine("Washers (1/4)", 24, new BigDecimal(".15")));
