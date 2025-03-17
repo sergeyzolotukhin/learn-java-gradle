4b3bbbfc3afe548cdd34b5d0f1aab243aab4b7d7
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-03-17 23:41:41 +0200

    spring SimpleStateMachine

--- learn-java/framework/spring/spring-xml-circular-dependency/src/main/java/ua/in/sz/circular/dependency/lazy/LazyCircularDependencyMain.java
+++ learn-java/framework/spring/spring-xml-circular-dependency/src/main/java/ua/in/sz/circular/dependency/lazy/LazyCircularDependencyMain.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ua.in.sz.circular.dependency.lazy.service.LazyPrintService;
 
 @Slf4j
-public class CircularDependencyLazyMain {
+public class LazyCircularDependencyMain {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext( "lazy/application-context.xml");
 
--- learn-java/framework/spring/spring-xml-circular-dependency/src/main/java/ua/in/sz/circular/dependency/lookup/LookupCircularDependencyMain.java
+++ learn-java/framework/spring/spring-xml-circular-dependency/src/main/java/ua/in/sz/circular/dependency/lookup/LookupCircularDependencyMain.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ua.in.sz.circular.dependency.lookup.service.PrintService;
 
 @Slf4j
-public class CircularDependencyLookupMain {
+public class LookupCircularDependencyMain {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext( "lookup/application-context.xml");
 
