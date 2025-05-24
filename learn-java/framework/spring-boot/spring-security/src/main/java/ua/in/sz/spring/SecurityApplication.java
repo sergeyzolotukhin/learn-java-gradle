7a5c03f941720553ca2a9551384c57fe988c6109
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-05-24 14:40:18 +0300

    spring SimpleStateMachine

--- learn-java/framework/spring-boot/spring-security/src/main/java/ua/in/sz/spring/SecurityApplication.java
+++ learn-java/framework/spring-boot/spring-security/src/main/java/ua/in/sz/spring/SecurityApplication.java
@@ -2,18 +2,12 @@ package ua.in.sz.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 //@EnableWebSecurity(debug = true)
 @SpringBootApplication
-public class Application {
+public class SecurityApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(SecurityApplication.class, args);
     }
 
 //    @Bean
