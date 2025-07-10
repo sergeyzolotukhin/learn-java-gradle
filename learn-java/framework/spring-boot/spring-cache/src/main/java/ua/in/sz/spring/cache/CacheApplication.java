2311e54fe4b25f62c6eca926d376ce2e663419f3
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-07-10 21:21:40 +0300

    Liquibase + spring data jpa

--- learn-java/framework/spring-boot/spring-cache/src/main/java/ua/in/sz/spring/cache/CacheApplication.java
+++ learn-java/framework/spring-boot/spring-cache/src/main/java/ua/in/sz/spring/cache/CacheApplication.java
@@ -10,8 +10,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication
-public class Application {
+public class CacheApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(CacheApplication.class, args);
 	}
 }
