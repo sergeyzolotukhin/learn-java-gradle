753e0c18ce54ebc04e01a53f3fb468969e05af7a
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-08-14 22:53:11 +0300

    spring actuator

---
+++ learn-java/framework/spring-boot/spring-boot-autoconfigure/greeter-spring-boot-autoconfigure/src/main/java/ua/in/sz/spring/autoconfig/greeter/GreetingConfig.java
@@ -0,0 +1,11 @@
+package ua.in.sz.spring.autoconfig.greeter;
+
+import java.io.Serial;
+import java.util.Properties;
+
+public class GreetingConfig extends Properties {
+
+    @Serial
+    private static final long serialVersionUID = 5662570853707247891L;
+
+}
