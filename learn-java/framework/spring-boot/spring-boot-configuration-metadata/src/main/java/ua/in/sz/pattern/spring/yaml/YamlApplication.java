b1d925a1343018a3ee4535442d4ef5c544b3d963
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-08-14 20:23:14 +0300

    spring actuator

--- learn-java/framework/spring-boot/spring-boot-configuration-metadata/src/main/java/ua/in/sz/pattern/spring/yaml/YamlApplication.java
+++ learn-java/framework/spring-boot/spring-boot-configuration-metadata/src/main/java/ua/in/sz/pattern/spring/yaml/YamlApplication.java
@@ -1,6 +1,8 @@
 package ua.in.sz.pattern.spring.yaml;
 
+import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,6 +11,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+
+import java.util.Map;
 
 // https://docs.spring.io/spring-boot/docs/2.1.7.RELEASE/reference/html/boot-features-external-config.html
 // https://www.baeldung.com/spring-yaml-propertysource
@@ -23,6 +27,10 @@ import org.springframework.context.annotation.PropertySources;
 public class YamlApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(YamlApplication.class, args);
+
+//        SpringApplication application = new SpringApplication(YamlApplication.class);
+//        application.setDefaultProperties(Map.of("acme.name", "Acme"));
+//        ConfigurableApplicationContext context = application.run(args);
 
         YamlBusinessService bean = context.getBean(YamlBusinessService.class);
         bean.print();
