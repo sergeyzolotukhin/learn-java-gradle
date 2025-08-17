fa9865a63cce8bcc5a51a1c48d66766b4722e617
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-08-17 19:08:28 +0300

    spring actuator

--- learn-java/library/lib-tomcat-websocket/src/main/java/ua/in/sz/tomcat/websocket/WebSocketEndpoint.java
+++ learn-java/library/lib-tomcat-websocket/src/main/java/ua/in/sz/tomcat/websocket/WebSocketEndpoint.java
@@ -2,18 +2,20 @@ package ua.in.sz.tomcat.websocket;
 
 import jakarta.websocket.OnMessage;
 import jakarta.websocket.server.ServerEndpoint;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @ServerEndpoint("/socket")
 public class WebSocketEndpoint {
     @OnMessage
     public String handleTextMessage(String message) {
-        System.out.println("New Text Message Received");
+        log.info("New Text Message Received");
         return message;
     }
 
     @OnMessage(maxMessageSize = 1024000)
     public byte[] handleBinaryMessage(byte[] buffer) {
-        System.out.println("New Binary Message Received");
+        log.info("New Binary Message Received");
         return buffer;
     }
 }
