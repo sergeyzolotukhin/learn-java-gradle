38cfb29007b59ebfc6c21a6dc311ff65cb142e6f
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-08-17 18:15:08 +0300

    spring actuator

---
+++ learn-java/library/lib-tomcat-websocket/TODO.md
@@ -0,0 +1,4 @@
+#### TODO
+
+https://dev.to/vonagedev/creating-a-websocket-server-with-the-java-api-for-websockets-1o1k
+
---
+++ learn-java/library/lib-tomcat-websocket/src/main/java/ua/in/sz/tomcat/websocket/TomcatEebsocketMain.java
@@ -0,0 +1,28 @@
+package ua.in.sz.tomcat.websocket;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+import java.io.File;
+
+@Slf4j
+public class TomcatEebsocketMain {
+    public static void main(String[] args) throws Exception {
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir(".tomcat");
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+        String contextPath = "./learn-java/library/lib-tomcat-websocket/src/main/webapp/";
+        String docBase = new File(contextPath).getAbsolutePath();
+
+        tomcat.addWebapp("/", docBase);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
