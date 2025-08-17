8654e867702dc21386e3decc873584e9b328f648
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-08-17 19:20:28 +0300

    spring actuator

--- learn-java/library/lib-tomcat-websocket/src/main/java/ua/in/sz/tomcat/websocket/WebSocketEndpoint.java
+++ learn-java/library/lib-tomcat-websocket/src/main/java/ua/in/sz/tomcat/websocket/WebSocketEndpoint.java
@@ -1,21 +1,34 @@
 package ua.in.sz.tomcat.websocket;
 
 import jakarta.websocket.OnMessage;
+import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @Slf4j
 @ServerEndpoint("/socket")
 public class WebSocketEndpoint {
     @OnMessage
-    public String handleTextMessage(String message) {
-        log.info("New Text Message Received");
-        return message;
-    }
+    public void handleTextMessage(String message, final Session session) {
+        log.info("Received message: {}", message);
 
-    @OnMessage(maxMessageSize = 1024000)
-    public byte[] handleBinaryMessage(byte[] buffer) {
-        log.info("New Binary Message Received");
-        return buffer;
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    log.info("Send message Push");
+                    session.getBasicRemote().sendText("Push");
+                } catch (IOException ex) {
+                    log.error("Can not sent message: ", ex);
+                }
+            }
+        };
+
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(timerTask, 0, 3 * 1000);
     }
 }
