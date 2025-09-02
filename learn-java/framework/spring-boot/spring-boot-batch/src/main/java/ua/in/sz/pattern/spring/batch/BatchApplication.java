7fddbf844a6eba36ad923c519eff1333d596e3d8
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-09-02 19:15:14 +0300

    spring actuator

--- learn-java/framework/spring-boot/spring-boot-batch/src/main/java/ua/in/sz/pattern/spring/batch/BatchApplication.java
+++ learn-java/framework/spring-boot/spring-boot-batch/src/main/java/ua/in/sz/pattern/spring/batch/BatchApplication.java
@@ -4,14 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @Slf4j
-@EnableBatchProcessing
 @SpringBootApplication
 public class BatchApplication {
     public static void main(String[] args) {
