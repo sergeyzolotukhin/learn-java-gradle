f70c32931200dea79c280b75837b846d209fe499
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-09-07 21:26:34 +0300

    spring actuator

--- learn-java/framework/spring-boot/spring-property/src/main/java/ua/in/sz/pattern/spring/location/LocationPropertyApplication.java
+++ learn-java/framework/spring-boot/spring-property/src/main/java/ua/in/sz/pattern/spring/location/LocationPropertyApplication.java
@@ -3,7 +3,9 @@ package ua.in.sz.pattern.spring.location;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.PropertySource;
 
 @Slf4j
 @Configuration
@@ -11,6 +13,15 @@ import org.springframework.context.annotation.Configuration;
 public class LocationPropertyApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(LocationPropertyApplication.class);
-        application.run(args);
+        ConfigurableApplicationContext context = application.run(args);
+
+//        context.getEnvironment().getProperty("");
+
+        for (PropertySource<?> propertySource : context.getEnvironment().getPropertySources()) {
+            String name = propertySource.getName();
+            Object source = propertySource.getSource();
+
+            log.info("Name: {}, source: {}", name, source.getClass().getSimpleName());
+        }
     }
 }
