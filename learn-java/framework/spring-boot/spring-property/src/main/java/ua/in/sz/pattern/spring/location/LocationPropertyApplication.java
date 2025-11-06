b6b3e5ab87088e35bc3563d7afd7d8bfaf7433d3
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-11-06 22:11:58 +0200

    spring actuator

--- learn-java/framework/spring-boot/spring-property/src/main/java/ua/in/sz/pattern/spring/location/LocationPropertyApplication.java
+++ learn-java/framework/spring-boot/spring-property/src/main/java/ua/in/sz/pattern/spring/location/LocationPropertyApplication.java
@@ -7,12 +7,15 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.PropertySource;
 
+import java.util.Map;
+
 @Slf4j
 @Configuration
 @SpringBootApplication
 public class LocationPropertyApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(LocationPropertyApplication.class);
+        application.setDefaultProperties(Map.of("spring.config.name", "application,default" ));
         ConfigurableApplicationContext context = application.run(args);
 
 //        context.getEnvironment().getProperty("");
