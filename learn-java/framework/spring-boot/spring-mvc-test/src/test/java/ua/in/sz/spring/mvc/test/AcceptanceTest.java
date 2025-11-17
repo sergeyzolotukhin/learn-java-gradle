049684b33c827735eca501ea8de8c17fb151b9f3
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-11-18 01:15:22 +0200

    spring-cloud-starter-config

--- learn-java/framework/spring-boot/spring-mvc-test/src/test/java/ua/in/sz/spring/mvc/test/AcceptanceTest.java
+++ learn-java/framework/spring-boot/spring-mvc-test/src/test/java/ua/in/sz/spring/mvc/test/AcceptanceTest.java
@@ -1,25 +1,45 @@
 package ua.in.sz.spring.mvc.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class AcceptanceTest {
+public class AcceptanceTest implements ApplicationContextAware {
     @LocalServerPort
     private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
 
+    private ApplicationContext applicationContext;
+
     @Test
     void greetingShouldReturnDefaultMessage() {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/", String.class))
                 .isEqualTo("Hello, ua.in.sz.spring.mvc.test.BusinessService");
+
+        log.info("\n\n\n");
+        for (String name : Arrays.stream(applicationContext.getBeanDefinitionNames()).sorted().toList()) {
+            log.info("Bean name: {}", name);
+        }
+        log.info("\n\n\n");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
--- learn-java/framework/spring-boot/spring-mvc-test/src/test/java/ua/in/sz/spring/mvc/test/WebLayerTest.java
+++ learn-java/framework/spring-boot/spring-mvc-test/src/test/java/ua/in/sz/spring/mvc/test/WebLayerTest.java
@@ -1,10 +1,16 @@
 package ua.in.sz.spring.mvc.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
@@ -13,12 +19,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Slf4j
 @WebMvcTest(HomeController.class)
-public class WebLayerTest {
+public class WebLayerTest implements ApplicationContextAware {
     @Autowired
     private MockMvc mockMvc;
     @MockitoBean
     private BusinessService service;
+    private ApplicationContext applicationContext;
 
     @Test
     void shouldReturnDefaultMessage() throws Exception {
@@ -28,5 +36,16 @@ public class WebLayerTest {
                 .andDo(log())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello, Mock NAME")));
+
+        log.info("\n\n\n");
+        for (String name : Arrays.stream(applicationContext.getBeanDefinitionNames()).sorted().toList()) {
+            log.info("Bean name: {}", name);
+        }
+        log.info("\n\n\n");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
