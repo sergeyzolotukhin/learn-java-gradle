0cadfb8474b6b1c3c957f7321b659ad1f65318c0
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-11-24 19:23:44 +0200

    spring-cloud-starter-config

--- learn-java/framework/spring-boot/spring-boot-actuator/src/main/java/ua/in/sz/pattern/spring/actuator/ActuatorController.java
+++ learn-java/framework/spring-boot/spring-boot-actuator/src/main/java/ua/in/sz/pattern/spring/actuator/ActuatorController.java
@@ -2,16 +2,10 @@ package ua.in.sz.pattern.spring.actuator;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ActuatorController {
-
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/hello-world")
 	@ResponseBody
