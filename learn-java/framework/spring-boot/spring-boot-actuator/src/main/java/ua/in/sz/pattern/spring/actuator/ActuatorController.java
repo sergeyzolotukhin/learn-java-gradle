bc9afb13d37aee6dda4c7382a7e2e82788f45444
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-11-24 20:27:22 +0200

    spring-cloud-starter-config

--- learn-java/framework/spring-boot/spring-boot-actuator/src/main/java/ua/in/sz/pattern/spring/actuator/ActuatorController.java
+++ learn-java/framework/spring-boot/spring-boot-actuator/src/main/java/ua/in/sz/pattern/spring/actuator/ActuatorController.java
@@ -1,5 +1,6 @@
 package ua.in.sz.pattern.spring.actuator;
 
+import io.micrometer.core.annotation.Timed;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +10,7 @@ public class ActuatorController {
 
 	@GetMapping("/hello-world")
 	@ResponseBody
+    @Timed(value = "request.greeting", extraTags = {"env", "local"})
 	public Greeting sayHello() {
 		return new Greeting("Hello, Serhij!");
 	}
