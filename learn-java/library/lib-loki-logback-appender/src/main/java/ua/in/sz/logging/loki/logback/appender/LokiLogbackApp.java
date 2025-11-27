0b93e3036f57fc0949df2570d3f34371a381c661
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-11-27 21:26:12 +0200

    spring-boot jaeger

--- learn-java/library/lib-loki-logback-appender/src/main/java/ua/in/sz/logging/loki/logback/appender/LokiLogbackApp.java
+++ learn-java/library/lib-loki-logback-appender/src/main/java/ua/in/sz/logging/loki/logback/appender/LokiLogbackApp.java
@@ -11,11 +11,19 @@ public class LokiLogbackApp {
 
     @SneakyThrows
     public static void main(String[] args) {
-        while (true) {
-            for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 100; j++) {
+            for (int i = 0; i < 1000; i++) {
                 log.info("Hello World! {}", UUID.randomUUID());
+
+                if (i % 10 == 0) {
+                    log.warn("Hello Serhij! {}", UUID.randomUUID());
+                }
+
+                TimeUnit.MICROSECONDS.sleep(50);
             }
-            TimeUnit.SECONDS.sleep(15);
+
+            log.error("Hello Serhij! {}", UUID.randomUUID());
+            TimeUnit.SECONDS.sleep(1);
         }
     }
 }
